6ec5221bbe52e35f3337ad5dc6aaec7a53440276
Kevin Miller <kevin@csumb.edu>
2018-09-10 11:19:31 -0700

    Removed some sample things.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,24 +7,8 @@ import './index.css'
 
 const Layout = ({ children, data }) => (
   <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
       {children()}
-    </div>
   </div>
 )
 
