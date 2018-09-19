c81206da9e26a98313cf1269e6dadef48c84e9da
Kevin Miller <kevin@csumb.edu>
2018-09-19 15:12:50 -0700

    Got something to filter!

--- src/pages/academics.js
+++ src/pages/academics.js
@@ -2,26 +2,72 @@ import React from 'react'
 import Layout from '../components/layouts/default'
 import Link from 'gatsby-link'
 import { graphql } from "gatsby"
+import { navigate } from "@reach/router"
 
-const AcademicsFilterForm = () => {
-  return (
-    <form>
-      <input type="text" placeholder="Search"/>
-    </form>
-  )
-}
 class AcademicsPage extends React.Component {
-  
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: '',
+      filteredItems: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    navigate(`?f=${this.state.filter}`)
+
+    if(this.state.filter.trim().length < 3) {
+      return;
+    }
+    //Filter all the rows from the sheet and add them to the component's filteredItems state
+    var filteredItems = []
+    this.props.data.allGoogleSheetSheet1Row.edges.map((row) => {
+      if(row.node.name.toLowerCase().search(this.state.filter) > -1) {
+        filteredItems.push(row.node)
+      } 
+      return filteredItems
+    })
+    this.setState({
+      filteredItems: filteredItems
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      filter: event.target.value
+    });
+  }
+
+  AcademicsResults(items) {
+    return (
+      <ul>
+      {items.map((result) => (
+        <li>{result.name}</li>
+      ))}
+      </ul>
+    )
+  }
   
   render() {
     const sheets = this.props.data.allGoogleSheetSheet1Row;
+
     return (
       <Layout>
         <h1>Academics</h1>
-        <AcademicsFilterForm/>
-        {sheets.edges.map((row, i) => (
-          <p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Search" onChange={this.handleChange}/>
+          <input type="submit" value="Search"/>
+        </form>
+        {this.state.filteredItems.length ?
+          this.AcademicsResults(this.state.filteredItems) :
+          null
+        }
+        {sheets.edges.map((row) => (
+          <p key={row.node.id}>
             <Link to={row.node.link.replace('https://csumb.edu/', '/')}>
               {row.node.name}
             </Link>
