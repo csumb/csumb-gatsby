533700c245d422c956e032cf6d96680c4f2259b3
Kevin Miller <kevin@csumb.edu>
2018-09-20 16:24:24 -0700

    Added footer links and grid system

--- src/components/layouts/partials/footer.js
+++ src/components/layouts/partials/footer.js
@@ -3,14 +3,68 @@ import React from 'react'
 import srOnly from '../../styles/sronly'
 import colors from '../../styles/colors'
 import Link from 'gatsby-link'
+import { Flex, Box } from '@rebass/grid/emotion'
 
 import { css } from 'emotion';
 
 const Footer = ({ children, data }) => (
   <footer className={css`
     background-color: ${colors.tan.light};
+    h2 {
+      color: ${colors.tan.dark};
+    }
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
     `}>
-    THE FOOTER
+    <Flex>
+      <Box width={[
+        1,
+        1,
+        1/4,
+        1/4
+      ]}>
+      <h2>Popular</h2>
+      <ul>
+        <li><Link to="http://csumb.edu/jobs" title="">Jobs</Link></li>
+        <li><Link to="https://csumb.edu/admissions/visit" title="">Tours</Link></li>
+        <li><Link to="/academics" title="">Majors</Link></li>
+        <li><Link to="http://csumb.edu/social" title="">Social media</Link></li>
+        <li><Link to="/library" title="">Library</Link></li>
+      </ul>
+      </Box>
+      <Box width={[
+        1,
+        1,
+        1/4,
+        1/4
+      ]}>
+      <h2>Tools</h2>
+      <ul>
+        <li><Link to="/map" title="">Map</Link></li>
+        <li><Link to="/mastercalendar" title="">Calendar</Link></li>
+        <li><Link to="/directory" title="">Directory</Link></li>
+        <li><Link to="/catalog" title="">Catalog</Link></li>
+        <li><Link to="/planning/schedule" title="">Class schedule</Link></li>
+      </ul>
+      </Box>
+      <Box width={[
+        1,
+        1,
+        1/4,
+        1/4
+      ]}>
+      <h2>Links for</h2>
+      <ul>
+        <li><Link to="/families" title="">Parents</Link></li>
+        <li><Link to="/international" title="">International students</Link></li>
+        <li><Link to="/locals" title="">Locals</Link></li>
+        <li><Link to="/veterans" title="">Veterans</Link></li>
+        <li><Link to="/employees" title="">Employees</Link></li>
+      </ul>
+      </Box>
+    </Flex>
     <div className={css`
       background-color: ${colors.tan.darkest};
       a {
