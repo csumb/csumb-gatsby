2066b6689fc90140e74b9735f8db99aab318e5cd
Kevin Miller <kevin@csumb.edu>
2018-09-21 13:54:51 -0700

    Changed bp for container

--- src/components/container.js
+++ src/components/container.js
@@ -6,7 +6,7 @@ const ContainerClass = css(bp({
   marginLeft: 15,
   marginRight: 15,
   margin: [0, 'auto'],
-  width: ['auto', 'auto', 700, 1100]
+  width: ['auto', 'auto', 'auto', 1100]
 }))
 
 const Container = ({ children }) => (
