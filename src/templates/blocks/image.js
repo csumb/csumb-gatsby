ece4b2509eb3ca0e77e888d4104b3691ca723b1f
Kevin Miller <kevin@csumb.edu>
2018-09-26 15:49:50 -0700

    Added CSS back to images.

--- src/templates/blocks/image.js
+++ src/templates/blocks/image.js
@@ -4,8 +4,17 @@ import { css } from 'emotion'
 class BlockImage extends React.Component {
 
   render() {
+    let imageClass = {}
+    if(this.props.block.data.pullRight) {
+      imageClass.float = 'right'
+    }
+    const imageClassName = css(imageClass)
     return (
-      <img src={this.props.block.data.image.url} alt={this.props.block.data.description}/>
+      <div className={imageClassName}>
+        <img className={css`
+          width: 100%;
+        `} src={this.props.block.data.image.url} alt={this.props.block.data.description}/>
+      </div>
     )
   }
 }
