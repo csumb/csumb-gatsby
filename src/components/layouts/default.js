1d29555ec901028bc09d913ca1e12667749e35a9
Kevin Miller <kevin@csumb.edu>
2018-10-09 16:30:44 -0700

    Changed to emotion themes

--- src/components/layouts/default.js
+++ src/components/layouts/default.js
@@ -6,44 +6,48 @@ import Header from './components/header'
 import Footer from './components/footer'
 import Container from '../container'
 import Helmet from 'react-helmet'
+import { ThemeProvider } from 'emotion-theming'
+import theme from '../styles/theme'
 
 class Layout extends React.Component {
   
   render() {
 
     return (
-      <div>
-        <Helmet>
-          <title>{typeof this.props.pageTitle !== 'undefined' ? this.props.pageTitle + ' | ' : null}Cal State Monterey Bay</title>
-        </Helmet>
-        <StaticQuery
-          query={graphql`{
-              site {
-                siteMetadata {
-                  swiftypeId
-                  title
-                  okta {
-                    login
+      <ThemeProvider theme={theme}>
+        <>
+          <Helmet>
+            <title>{typeof this.props.pageTitle !== 'undefined' ? this.props.pageTitle + ' | ' : null}Cal State Monterey Bay</title>
+          </Helmet>
+          <StaticQuery
+            query={graphql`{
+                site {
+                  siteMetadata {
+                    swiftypeId
+                    title
+                    okta {
+                      login
+                    }
                   }
                 }
-              }
-            }`}
-            render={data => (
-                  <>
-                  <Header metadata={data.site.siteMetadata}/>
-                  
-                  </>
-                  )}
-        />
-        <div className={css`
-        clear: both 
-        `}>
-        <Container>
-          {this.children}
-        </Container>
-        </div>
-        <Footer/>
-      </div>
+              }`}
+              render={data => (
+                    <>
+                    <Header metadata={data.site.siteMetadata}/>
+                    
+                    </>
+                    )}
+          />
+          <div className={css`
+          clear: both 
+          `}>
+          <Container>
+            {this.props.children}
+          </Container>
+          </div>
+          <Footer/>
+        </>
+      </ThemeProvider>
     )
   }
 }
