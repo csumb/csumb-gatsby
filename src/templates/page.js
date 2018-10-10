a35cc973028c49b90f9c1f92c296a4074093fd92
Kevin Miller <kevin@csumb.edu>
2018-10-10 08:58:21 -0700

    Page context

--- src/templates/page.js
+++ src/templates/page.js
@@ -8,7 +8,7 @@ class PageTemplate extends React.Component {
   
   render() {
     return (
-      <Layout pageTitle={this.props.pageContent.title}>
+      <Layout pageTitle={this.props.pageContext.title}>
         <SiteNavigation navigation={this.props.pageContext.navigation}/>
         <h1>hi there {this.props.pageContext.title} in site {this.props.pageContext.site.title}</h1>
         <Blocks blocks={this.props.pageContext.pageContent}></Blocks>  
