9855fecd80abbe26010af56fe7d184f982d59a04
Kevin Miller <kevin@csumb.edu>
2018-10-24 14:40:51 -0700

    Nav on the side

--- src/templates/page.js
+++ src/templates/page.js
@@ -7,6 +7,7 @@ import SiteHeader from '../components/layouts/components/site-header'
 import Container from '../components/container'
 import PageTitle from '../components/page-title'
 import Breadcrumbs from '../components/breadcrumbs'
+import { Flex, Box } from '@rebass/grid/emotion'
 
 class PageTemplate extends React.Component {
   render() {
@@ -16,13 +17,19 @@ class PageTemplate extends React.Component {
           title={this.props.pageContext.site.title}
           path={this.props.pageContext.site.site}
         />
-        <SiteNavigation navigation={this.props.pageContext.navigation} />
         <Container>
           <Breadcrumbs breadcrumbs={this.props.pageContext.breadcrumbs} />
-          <PageTitle layout={this.props.pageContext.layout}>
-            {this.props.pageContext.title}
-          </PageTitle>
-          <Blocks blocks={this.props.pageContext.pageContent} />
+          <Flex flexWrap="wrap">
+            <Box width={[1, 1, 1 / 5, 1 / 5]} px={2}>
+              <SiteNavigation navigation={this.props.pageContext.navigation} />
+            </Box>
+            <Box width={[1, 1, 4 / 5, 4 / 5]} px={2}>
+              <PageTitle layout={this.props.pageContext.layout}>
+                {this.props.pageContext.title}
+              </PageTitle>
+              <Blocks blocks={this.props.pageContext.pageContent} />
+            </Box>
+          </Flex>
         </Container>
       </Layout>
     )
