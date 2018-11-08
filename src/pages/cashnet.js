9aa58ac8f58b4e5c6476776816e4ae429e08cd71
Kevin Miller <kevin@csumb.edu>
2018-11-08 12:11:53 -0800

    Made cashnet handle auth by itself.

--- src/pages/cashnet.js
+++ src/pages/cashnet.js
@@ -1,28 +1,40 @@
 import React from 'react'
 import { UserContext } from 'components/contexts/user'
 
-const Cashnet = props => {
-  return (
-    <UserContext.Consumer>
-      {context => (
-        <>
-          {context.user &&
-            context.user !== 'anonymous' && (
-              <>
-                <h1>One sec, we're redirecting you to CashNet...</h1>
-                <div style={{ display: 'none' }}>
-                  {
-                    (window.location = `https://api.csumb.edu/cashnet/${
-                      context.user.profile.employeeNumber
-                    }/RMBRD`)
-                  }
-                </div>
-              </>
-            )}
-        </>
-      )}
-    </UserContext.Consumer>
-  )
+class CashnetPage extends React.Component {
+  state = {
+    user: false,
+  }
+  componentDidMount() {
+    window
+      .fetch('https://csumb.okta.com/api/v1/users/me', {
+        credentials: 'include',
+      })
+      .then(response => {
+        return response.json()
+      })
+      .then(user => {
+        window.location = `https://api.csumb.edu/cashnet/${
+          user.profile.employeeNumber
+        }/RMBRD`
+      })
+      .catch(error => {
+        this.setState({
+          user: 'anonymous',
+        })
+      })
+  }
+  render() {
+    return (
+      <>
+        {this.state.user === 'anonymous' ? (
+          <h1>You must be logged in</h1>
+        ) : (
+          <h1>One sec, we're redirecting you to CashNet...</h1>
+        )}
+      </>
+    )
+  }
 }
 
-export default Cashnet
+export default CashnetPage
