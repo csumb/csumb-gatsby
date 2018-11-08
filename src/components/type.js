74fd546d392ffd678372bef7ea88e37edb4f3a43
Kevin Miller <kevin@csumb.edu>
2018-11-08 11:16:38 -0800

    Handle multiple applications

---
+++ src/components/type.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'react-emotion'
+
+const LeadParagraph = styled('p')`
+  font-size: 1.3rem;
+`
+
+export { LeadParagraph }
--- src/pages/account/applicant-status.js
+++ src/pages/account/applicant-status.js
@@ -6,6 +6,9 @@ import { Flex, Box } from '@rebass/grid/emotion'
 import { AccountGroup, AccountTitle, AccountSidebar } from 'components/account'
 import { UserContext } from 'components/contexts/user'
 import { ButtonLink } from 'components/button'
+import url from 'url'
+import { LeadParagraph } from 'components/type'
+import Link from 'gatsby-link'
 
 class AccountApplicantStatusPage extends React.Component {
   render() {
@@ -26,21 +29,13 @@ class AccountApplicantStatusPage extends React.Component {
                       </>
                     )}
                   </PageTitle>
-                  <Flex flexWrap="wrap">
-                    <Box width={[1, 1, 1 / 4, 1 / 4]} px={2}>
-                      <AccountSidebar user={context.user} />
-                    </Box>
-                    <Box width={[1, 1, 3 / 4, 3 / 4]} px={2}>
-                      {context.user === 'anonymous' ? (
-                        <h3>You must be logged in first.</h3>
-                      ) : (
-                        <>
-                          <AccountTitle>Application Status</AccountTitle>
-                          <ApplicantStatus user={context.user} />
-                        </>
-                      )}
-                    </Box>
-                  </Flex>
+                  {context.user === 'anonymous' ? (
+                    <h3>You must be logged in first.</h3>
+                  ) : (
+                    <>
+                      <ApplicantStatus user={context.user} />
+                    </>
+                  )}
                 </>
               )}
             </Container>
@@ -61,9 +56,31 @@ const Application = props => (
   </AccountGroup>
 )
 
+const ApplicationMultipleMessage = props => (
+  <>
+    {props.applications &&
+      props.applications.length && (
+        <LeadParagraph>
+          You have more than one application:{' '}
+          {props.applications.map(application => (
+            <Link
+              key={application.application.application_number}
+              to={`account/applicant-status?app=${
+                application.application.application_number
+              }`}
+            >
+              {application.term.gsx$name}{' '}
+            </Link>
+          ))}
+        </LeadParagraph>
+      )}
+  </>
+)
+
 class ApplicantStatus extends React.Component {
   state = {
-    application: false,
+    applications: false,
+    currentApplication: false,
   }
 
   componentDidMount() {
@@ -73,13 +90,29 @@ class ApplicantStatus extends React.Component {
         return response.json()
       })
       .then(applications => {
+        applications = this.processApplications(applications)
+        let current = 0
+        if (applications && window.location.search.search('app=') > -1) {
+          let location = url.parse(window.location.href, true)
+          if (location.query && typeof location.query.app !== 'undefined') {
+            applications.forEach((application, index) => {
+              console.log(application.application)
+              if (
+                application.application.application_number == location.query.app
+              ) {
+                current = index
+              }
+            })
+          }
+        }
         this.setState({
-          application: this.processApplications(applications),
+          applications: applications,
+          currentApplication: current,
         })
       })
       .catch(error => {
         this.setState({
-          application: 'empty',
+          applications: 'empty',
         })
       })
   }
@@ -106,13 +139,16 @@ class ApplicantStatus extends React.Component {
   render() {
     return (
       <>
-        {!this.state.application ? (
+        {!this.state.applications ? (
           <p>Loading applications</p>
         ) : (
           <>
-            {this.state.application.map(application => (
-              <Application {...application} />
-            ))}
+            <ApplicationMultipleMessage
+              applications={this.state.applications}
+            />
+            <Application
+              {...this.state.applications[this.state.currentApplication]}
+            />
           </>
         )}
       </>
