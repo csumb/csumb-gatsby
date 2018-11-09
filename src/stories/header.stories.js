86e30e9b3c431280d28b3cd94a3f8c0f136a8ed2
Kevin Miller <kevin@csumb.edu>
2018-11-08 18:36:18 -0800

    Completed header stories

--- src/stories/header.stories.js
+++ src/stories/header.stories.js
@@ -2,15 +2,45 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Brand from 'components/layouts/components/brand'
 import Applicant from 'components/layouts/components/applicant'
+import Header from 'components/layouts/components/header'
+import {
+  NavigationLink,
+  NavigationLinkApply,
+} from 'components/layouts/components/navigation-link'
 import typography from 'utils/typography'
 import { UserContext } from 'components/contexts/user'
 
 typography.injectStyles()
 
 storiesOf('Header', module)
+  .add('About', () => (
+    <p>
+      The header uses state to change the layout of the design based on screen
+      size.
+    </p>
+  ))
+  .add('Complete Header', () => (
+    <UserContext.Provider value={{ user: true }}>
+      <Header
+        metadata={{
+          title: `Cal State Monterey Bay`,
+          okta: {
+            login: `https://csumb.okta.com`,
+          },
+          swiftypeId: `Gu7FdGTPV49T6dsYVBSV`,
+        }}
+      />
+    </UserContext.Provider>
+  ))
   .add('Brand', () => <Brand />)
   .add('Applicant link', () => (
     <UserContext.Provider value={{ user: true }}>
       <Applicant />
     </UserContext.Provider>
+  ))
+  .add('Navigation Link', () => (
+    <NavigationLink to="/about">About</NavigationLink>
+  ))
+  .add('Apply Navigation Link', () => (
+    <NavigationLinkApply to="/apply">Apply</NavigationLinkApply>
   ))
