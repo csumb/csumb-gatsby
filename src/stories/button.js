7564c537972715a788086491c78654956d54501d
Kevin Miller <kevin@csumb.edu>
2018-11-13 10:00:12 -0800

    Cleaned up some stories

--- src/stories/button.js
+++ src/stories/button.js
@@ -6,14 +6,11 @@ import typography from 'utils/typography'
 typography.injectStyles()
 
 storiesOf('ButtonLink', module)
-  .add('About', () => (
-    <p>
-      Button links are used for making a link to a page that evokes the feeling
-      of an action. Do not use button links to invoke Javascript or do something
-      that changes the state of the current page, use a regular button insted.
-    </p>
-  ))
-  .add('Default', () => <ButtonLink to="#here">A button</ButtonLink>)
+  .add('Default', () => <ButtonLink to="#here">A button</ButtonLink>, {
+    info: `Button links are used for making a link to a page that evokes the feeling
+    of an action. Do not use button links to invoke Javascript or do something
+    that changes the state of the current page, use a regular button insted.`,
+  })
   .add('Primary', () => (
     <ButtonLink to="#here" buttonType="primary">
       A primary button
@@ -26,13 +23,10 @@ storiesOf('ButtonLink', module)
   ))
 
 storiesOf('Button', module)
-  .add('About', () => (
-    <p>
-      While styled the same as Button Links, buttons are for in-page actions,
-      like popping open a modal or changing a value.
-    </p>
-  ))
-  .add('Default', () => <Button>A native button</Button>)
+  .add('Default', () => <Button>A native button</Button>, {
+    info: `While styled the same as Button Links, buttons are for in-page actions,
+    like popping open a modal or changing a value.`,
+  })
   .add('Primary', () => <Button buttonType="primary">A primary button</Button>)
   .add('High Impact', () => (
     <Button buttonType="highImpact">High impact</Button>
---
+++ src/stories/table.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Table, TableRow, TableHeader, TableCell } from 'components/table'
+import typography from 'utils/typography'
+
+typography.injectStyles()
+
+storiesOf('Table', module).add('Table', () => (
+  <Table>
+    <thead>
+      <TableRow>
+        <TableHeader>Header A</TableHeader>
+        <TableHeader>Header B</TableHeader>
+        <TableHeader>Header C</TableHeader>
+      </TableRow>
+    </thead>
+    <tbody>
+      <TableRow>
+        <TableCell>Item 1</TableCell>
+        <TableCell>Item 2</TableCell>
+        <TableCell>Item 3</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Item 1</TableCell>
+        <TableCell>Item 2</TableCell>
+        <TableCell>Item 3</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Item 1</TableCell>
+        <TableCell>Item 2</TableCell>
+        <TableCell>Item 3</TableCell>
+      </TableRow>
+    </tbody>
+  </Table>
+))
