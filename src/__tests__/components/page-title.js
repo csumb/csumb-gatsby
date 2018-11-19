257bc1fcba75da27ac39f8a13f288e5029b0d473
Kevin Miller <kevin@csumb.edu>
2018-11-19 12:41:22 -0800

    Added pagetitle tests

---
+++ src/__tests__/components/page-title.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import PageTitle from 'components/page-title'
+
+describe('Breadcrumbs', () => {
+  it('renders a page title', () => {
+    const tree = renderer
+      .create(<PageTitle layout="page">The page title</PageTitle>)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders sub titles for sites', () => {
+    const tree = renderer
+      .create(
+        <PageTitle layout="page" sub="Sub title">
+          The page title
+        </PageTitle>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders nothing for sites', () => {
+    const tree = renderer
+      .create(<PageTitle layout="site">The page title</PageTitle>)
+      .toJSON()
+    expect(tree).toBeNull()
+  })
+})
