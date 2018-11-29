ae2c8c131c6e4ddef1c5b06e7adf9f896fe67c34
Kevin Miller <kevin@csumb.edu>
2018-11-29 15:38:37 -0800

    Sunset course block as part of #2

---
+++ src/templates/blocks/blocks/courses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AlertEmpty } from 'components/alert'
+
+const CourseBlock = () => (
+  <AlertEmpty>
+    All course listings are now available in{' '}
+    <a href="/catalog">the university catalog</a>.
+  </AlertEmpty>
+)
+
+export default CourseBlock
