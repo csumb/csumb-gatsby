ac2d05651e06d73297ec37808d2550f6413c9db5
Kevin Miller <kevin@csumb.edu>
2018-12-20 13:32:03 -0800

    Hero image?

--- src/pages/everything.js
+++ src/pages/everything.js
@@ -6,6 +6,8 @@ import styled from 'react-emotion'
 import { colors } from 'components/styles/theme'
 import { Flex, Box } from '@rebass/grid/emotion'
 import SiteHeader from 'components/site-header'
+import LazyHero from 'react-lazy-hero'
+import heroImage from 'assets/hero-images/people/students-sunset-beach.jpg'
 
 const TopLevelList = styled('ul')`
   border-right: 1px solid ${colors.muted.dark};
@@ -63,6 +65,16 @@ const ThirdLevelList = styled('ul')`
   list-style-type: none;
 `
 
+const EverythingHero = () => (
+  <LazyHero
+    opacity={0}
+    parallaxOffset={0}
+    transitionDuration={0}
+    imageSrc={heroImage}
+    minHeight="250px">
+  </LazyHero>
+)
+
 class EverythingPage extends React.Component {
   state = {
     firstSelected: false,
@@ -103,8 +115,9 @@ class EverythingPage extends React.Component {
   render() {
     const { secondSelected, thirdSelected } = this.state
     return (
-      <Layout pageTitle="About">
+      <Layout pageTitle="Everything else">
         <SiteHeader path="/everything">Everything else</SiteHeader>
+        <EverythingHero />
         <Container topPadding>
           <Flex flexWrap="wrap">
             <Box width={[1, 1 / 5]} px={2}>
