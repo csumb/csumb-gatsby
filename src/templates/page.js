89a3f9df931cf498b71c99ea378c9ba990aadb3c
Kevin Miller <kevin@csumb.edu>
2018-12-21 14:25:22 -0800

    Moved reactive state to context

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 //import Link from 'gatsby'
-import Blocks from './blocks'
-import Layout from 'components/layouts/default'
-import SiteNavigation from 'components/site-navigation'
-import SiteHeader from 'components/site-header'
-import Container from 'components/container'
-import PageTitle from 'components/page-title'
-import { EventPage } from 'components/event'
-import Breadcrumbs from 'components/breadcrumbs'
+import Blocks from "./blocks";
+import Layout from "components/layouts/default";
+import SiteNavigation from "components/site-navigation";
+import SiteHeader from "components/site-header";
+import Container from "components/container";
+import PageTitle from "components/page-title";
+import { EventPage } from "components/event";
+import Breadcrumbs from "components/breadcrumbs";
 
 class PageTemplate extends React.Component {
   render() {
@@ -18,22 +18,22 @@ class PageTemplate extends React.Component {
       breadcrumbs,
       layout,
       event,
-      pageContent,
-    } = this.props.pageContext
+      pageContent
+    } = this.props.pageContext;
 
     return (
-      <Layout pageTitle={title}>
+      <Layout pageTitle={title} siteNavigation={navigation}>
         <SiteHeader path={site.site}>{site.title}</SiteHeader>
         <SiteNavigation navigation={navigation} />
         <Container>
           <Breadcrumbs breadcrumbs={breadcrumbs} />
-          {layout !== 'site' && <PageTitle layout={layout}>{title}</PageTitle>}
+          {layout !== "site" && <PageTitle layout={layout}>{title}</PageTitle>}
         </Container>
         {event && <EventPage event={event} />}
         <Blocks blocks={pageContent} />
       </Layout>
-    )
+    );
   }
 }
 
-export default PageTemplate
+export default PageTemplate;
