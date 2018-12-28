e1f454d8a1c8eb3cb7b9c66f7f80a3fc4d09e24a
Kevin Miller <kevin@csumb.edu>
2018-12-27 17:03:30 -0800

    Ignore immortal db

--- src/__tests__/components/course.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import { CourseListItem, CoursePage } from 'components/course'
-
-describe('Courses', () => {
-  it('stub', () => {
-    expect(CourseListItem).toBeTruthy()
-  })
-})
