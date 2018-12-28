0262bf6131bf4be06448b9be7b10e899e4ab6447
Kevin Miller <kevin@csumb.edu>
2018-12-27 16:18:00 -0800

    More standardized

--- src/templates/blocks/container-context.js
+++ src/templates/blocks/container-context.js
@@ -1,35 +1,36 @@
 import React from 'react'
 import bp from 'components/styles/breakpoints'
 import { css } from 'emotion'
+import { Box } from '@rebass/grid/emotion'
 import styled from 'react-emotion'
 
 const containerStyle = {
   normal: css(
     bp({
-      marginLeft: ['1rem', 'auto'],
-      marginRight: ['1rem', 'auto'],
-      width: ['100%', '100%', '66ch'],
+      marginLeft: ['1rem', '1rem', 'auto'],
+      marginRight: ['1rem', '1rem', 'auto'],
+      maxWidth: '66ch',
     })
   ),
   column: css(
     bp({
-      marginLeft: ['1rem', 'auto'],
-      marginRight: ['1rem', 'auto'],
+      marginLeft: ['1rem', '1rem', 'auto'],
+      marginRight: ['1rem', '1rem', 'auto'],
       width: ['100%'],
     })
   ),
   full: css(
     bp({
-      marginLeft: ['1rem', 'auto'],
-      marginRight: ['1rem', 'auto'],
-      width: ['auto', 'auto', 1100],
+      marginLeft: ['1rem', '1rem', 'auto'],
+      marginRight: ['1rem', '1rem', 'auto'],
+      maxWidth: '1100px',
     })
   ),
   slightlyLarger: css(
     bp({
-      marginLeft: ['1rem', 'auto'],
-      marginRight: ['1rem', 'auto'],
-      width: ['100%', '100%', '80ch'],
+      marginLeft: ['1rem', '1rem', 'auto'],
+      marginRight: ['1rem', '1rem', 'auto'],
+      maxWidth: '80ch',
     })
   ),
 }
