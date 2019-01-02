0f80458af084076b19e04eb7cfa6c427437fc291
Kevin Miller <kevin@csumb.edu>
2019-01-02 10:42:08 -0800

    Updated dependencies, npm audit

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure, addDecorator } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { withInfo } from '@storybook/addon-info'
+import '@babel/polyfill';
 
 addDecorator(
   withInfo({
@@ -10,8 +11,8 @@ addDecorator(
 // Gatsby's Link overrides:
 // Gatsby defines a global called ___loader to prevent its method calls from creating console errors we override it here
 global.___loader = {
-  enqueue: () => {},
-  hovering: () => {},
+  enqueue: () => { },
+  hovering: () => { },
 }
 // Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
 global.__PATH_PREFIX__ = ''
