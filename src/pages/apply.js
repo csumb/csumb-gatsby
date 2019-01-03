f23f36d3f548b4ed871091a08500f45cbe2935bf
Kevin Miller <kevin@csumb.edu>
2019-01-03 14:45:23 -0800

    Added static apply page

---
+++ src/pages/apply.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Layout from 'components/layouts/default'
+import { graphql } from 'gatsby'
+import SiteHeader from 'components/site-header'
+import PageTitle from 'components/page-title'
+import Container from 'components/container'
+
+const CopyrightPage = props => {
+  const page = props.data.allMarkdownRemark.edges[0].node
+  return (
+    <Layout pageTitle={page.frontmatter.title}>
+      <SiteHeader path="/admissions">Admissions</SiteHeader>
+      <Container>
+        <PageTitle>{page.frontmatter.title}</PageTitle>
+        <div dangerouslySetInnerHTML={{ __html: page.html }} />
+      </Container>
+    </Layout>
+  )
+}
+
+export default CopyrightPage
+
+export const query = graphql`
+  {
+    allMarkdownRemark(filter: { frontmatter: { name: { eq: "apply" } } }) {
+      edges {
+        node {
+          frontmatter {
+            name
+            title
+          }
+          html
+        }
+      }
+    }
+  }
+`
