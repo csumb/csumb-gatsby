3866170f1cde3a9dbfbdb0f161a036567b9fc612
Kevin Miller <kevin@csumb.edu>
2019-01-04 09:50:19 -0800

    Added upload ottercard

--- src/pages/account/card/upload.js
+++ src/pages/account/card/upload.js
@@ -3,41 +3,38 @@ import Layout from 'components/layouts/default'
 import SiteHeader from 'components/site-header'
 import Container from 'components/container'
 import PageTitle from 'components/page-title'
-import { AlertWarning } from 'components/alert'
+import { AlertWarning, AlertInfo } from 'components/alert'
 import { UserContext } from 'components/contexts/user'
 import { Flex, Box } from '@rebass/grid/emotion'
 import samplePhoto from 'assets/images/card-sample-photo.png'
 import styled from 'react-emotion'
 import { Button } from 'components/button'
-import ReactFilestack from 'filestack-react'
+import ReactFilestack, { client } from 'filestack-react'
 
 const SamplePhoto = styled('img')`
   float: right;
 `
 
+const PhotoRules = () => (
+  <ul>
+    <li>Think drivers license or passport photo.</li>
+    <li>Your eyes and face should be clearly visible.</li>
+    <li>The background should be solid.</li>
+    <li>You should be the only person in the picture.</li>
+    <li>Your entire face/head is visible in the photo.</li>
+    <li>No filters.</li>
+  </ul>
+)
+
 const UploadPreamble = () => (
   <Flex flexWrap="wrap">
     <Box width={[1, 2 / 3, 2 / 3]} px={2}>
       <p>
-        You can take a photo with your device or upload a
-        file of a headshot. You will be able to crop and
-        reposition your photo before saving it. A few
-        things to keep in mind:
-                            </p>
-      <ul>
-        <li>Think drivers license or passport photo.</li>
-        <li>
-          Your eyes and face should be clearly visible.
-                              </li>
-        <li>The background should be solid.</li>
-        <li>
-          You should be the only person in the picture.
-                              </li>
-        <li>
-          Your entire face/head is visible in the photo.
-                              </li>
-        <li>No filters.</li>
-      </ul>
+        You can take a photo with your device or upload a file of a headshot.
+        You will be able to crop and reposition your photo before saving it. A
+        few things to keep in mind:
+      </p>
+      <PhotoRules />
     </Box>
     <Box width={[1, 1 / 3, 1 / 3]} px={2}>
       <SamplePhoto src={samplePhoto} alt="Sample photo" />
@@ -45,18 +42,57 @@ const UploadPreamble = () => (
   </Flex>
 )
 
+const UploadCheck = ({ photo }) => (
+  <Flex flexWrap="wrap">
+    <Box width={[1, 2 / 3, 2 / 3]} px={2}>
+      <p>
+        This is how your picture will look on your OtterCard. Click confirm to
+        send your picture off to be printed. Please check that:
+      </p>
+      <PhotoRules />
+      <p>
+        I understand that if my photo does not meet these requirements, my CSUMB
+        student ID card may be delayed, and I may be charged a processing fee.
+      </p>
+    </Box>
+    <Box width={[1, 1 / 3, 1 / 3]} px={2}>
+      <SamplePhoto src={photo.url} alt="Sample photo" />
+    </Box>
+  </Flex>
+)
+
 class OtterCardPage extends React.Component {
   state = {
     photo: false,
-    showUpload: false
+    showUpload: false,
+    filePickerImage: false,
+    done: false
   }
 
   handleShowUpload(event) {
     event.preventDefault()
   }
 
+  handleUploadedPhoto(event) {
+    console.log(event)
+    this.setState({
+      photo: true,
+      showUpload: true,
+      filePickerImage: event.filesUploaded[0],
+    })
+  }
+
+  handleApprovedPhoto(user, image) {
+    const login = user.profile.login.split('@').shift()
+    client.storeUrl(image.url, {
+      filename: `${login}.jpg`,
+      path: `/${login}.jpg`,
+      location: 'dropbox',
+    })
+  }
+
   render() {
-    const { photo, showUpload } = this.state
+    const { filePickerImage, showUpload, done } = this.state
     return (
       <Layout pageTitle="OtterCard">
         <UserContext.Consumer>
@@ -70,30 +106,41 @@ class OtterCardPage extends React.Component {
                     <>
                       {context.user && !context.user.anonymous ? (
                         <>
-                          {showUpload ? (
-                            <>
-
-                            </>
+                          {done ? (
+                            <AlertInfo>Your photo has been received. We'll send you an email within a few days to confirm your card is ready.</AlertInfo>
                           ) : (
                               <>
-                                <UploadPreamble />
-                                <ReactFilestack
-                                  apikey="A3ttdsdUR8aGvjvUnJBWUz"
-                                  onSuccess={this.savePhoto}
-                                  options={{
-                                    accept: 'image/*',
-                                    maxFiles: 1,
-                                    storeTo: {
-                                      location: 's3',
-                                    },
-                                  }}
-                                  render={({ onPick }) => (
-                                    <div>
-                                      <Button onClick={onPick}>Select or take photo</Button>
-                                    </div>
+                                {showUpload ? (
+                                  <>
+                                    <UploadCheck photo={this.state.filePickerImage} />
+                                    <Button onClick={(event) => {
+                                      event.preventDefault()
+                                      this.handleApprovedPhoto(context.user, filePickerImage)
+                                      this.setState({
+                                        done: true
+                                      })
+                                    }}>Looks good!</Button>
+                                  </>
+                                ) : (
+                                    <>
+                                      <UploadPreamble />
+                                      <ReactFilestack
+                                        apikey="A3ttdsdUR8aGvjvUnJBWUz"
+                                        onSuccess={this.handleUploadedPhoto.bind(this)}
+                                        options={{
+                                          accept: 'image/*',
+                                          maxFiles: 1,
+                                        }}
+                                        render={({ onPick }) => (
+                                          <div>
+                                            <Button onClick={onPick}>
+                                              Select or take photo
+                                    </Button>
+                                          </div>
+                                        )}
+                                      />
+                                    </>
                                   )}
-                                />
-
                               </>
                             )}
                         </>
