1dfc439b1b36b5b94f91d63febbdc82b94d664a1
Kevin Miller <kevin@csumb.edu>
2019-01-07 10:48:44 -0800

    Made first load of everything page default to a grid view

--- src/pages/everything.js
+++ src/pages/everything.js
@@ -2,10 +2,21 @@ import React from 'react'
 import Layout from 'components/layouts/default'
 import Container from 'components/container'
 import { graphql } from 'gatsby'
+import { LinkyButton } from 'components/button'
 import styled from 'react-emotion'
-import { colors } from 'components/styles/theme'
+import { colors, fonts } from 'components/styles/theme'
 import { Flex, Box } from '@rebass/grid/emotion'
 import SiteHeader from 'components/header/site-header'
+
+const TopLevelBox = styled(Box)`
+  h4 {
+    margin: 0;
+    button {
+      font-family: ${fonts.sansSerif};
+    }
+  }
+  margin-bottom: 1rem;
+`
 
 const TopLevelList = styled('ul')`
   border-right: 1px solid ${colors.muted.dark};
@@ -42,6 +53,7 @@ const SecondLevelItem = styled('li')`
   h3 {
     margin: 0;
     color: ${colors.primary.dark};
+    font-family: ${fonts.sansSerif};
   }
   p {
     margin: 0;
@@ -63,11 +75,98 @@ const ThirdLevelList = styled('ul')`
   list-style-type: none;
 `
 
-class EverythingPage extends React.Component {
+class EverythingPageNavigation extends React.Component {
   state = {
-    firstSelected: false,
     secondSelected: false,
     thirdSelected: false,
+  }
+
+  componentDidMount() {
+    if (this.props.selectedItem) {
+      this.setState({
+        secondSelected: this.props.selectedItem,
+      })
+    }
+  }
+
+  handleSecondLevel(id, event) {
+    event.preventDefault()
+    this.setState({
+      secondSelected: id,
+      thirdSelected: false,
+    })
+  }
+
+  handleThirdLevel(id, event) {
+    event.preventDefault()
+    this.setState({
+      thirdSelected: id,
+    })
+  }
+
+  render() {
+    const { secondSelected, thirdSelected } = this.state
+    const { topLevelItems, navigation } = this.props
+    return (
+      <Flex flexWrap="wrap">
+        <Box width={[1, 1 / 5]} px={2}>
+          <TopLevelList>
+            {topLevelItems.map(item => (
+              <TopLevelItem key={item.contentful_id}>
+                <TopLevelItemButton
+                  onClick={this.handleSecondLevel.bind(
+                    this,
+                    item.contentful_id
+                  )}
+                  isActive={secondSelected === item.contentful_id}
+                >
+                  {item.title}
+                </TopLevelItemButton>
+              </TopLevelItem>
+            ))}
+          </TopLevelList>
+        </Box>
+        {secondSelected && (
+          <Box width={[1, 2 / 5]} px={2}>
+            <h2>{navigation[secondSelected].title}</h2>
+            <SecondLevelList>
+              {navigation[secondSelected].contentfulchildren.map(id => (
+                <SecondLevelItem key={id.contentful_id}>
+                  <HiddenButton
+                    onClick={this.handleThirdLevel.bind(this, id.contentful_id)}
+                  >
+                    <h3>{navigation[id.contentful_id].title}</h3>
+                    {navigation[id.contentful_id]
+                      .childContentfulNavigationItemDescriptionTextNode && (
+                      <p
+                        dangerouslySetInnerHTML={{
+                          __html:
+                            navigation[id.contentful_id]
+                              .childContentfulNavigationItemDescriptionTextNode
+                              .childMarkdownRemark.html,
+                        }}
+                      />
+                    )}
+                  </HiddenButton>
+                </SecondLevelItem>
+              ))}
+            </SecondLevelList>
+          </Box>
+        )}
+        {thirdSelected && (
+          <Box width={[1, 2 / 5]} px={2}>
+            <h2>{navigation[thirdSelected].title}</h2>
+            <ThirdLevelList />
+          </Box>
+        )}
+      </Flex>
+    )
+  }
+}
+
+class EverythingPage extends React.Component {
+  state = {
+    topLevelItem: false,
   }
 
   constructor(props) {
@@ -84,84 +183,51 @@ class EverythingPage extends React.Component {
     })
   }
 
-  handleSecondLevel(id, event) {
-    event.preventDefault()
-    this.setState({
-      secondSelected: id,
-      firstSelected: id,
-      thirdSelected: false,
-    })
-  }
-
-  handleThirdLevel(id, event) {
-    event.preventDefault()
-    this.setState({
-      thirdSelected: id,
-    })
-  }
-
   render() {
-    const { secondSelected, thirdSelected } = this.state
+    const { topLevelItem } = this.state
     return (
       <Layout pageTitle="Everything else">
         <SiteHeader path="/everything">Everything else</SiteHeader>
         <Container topPadding>
-          <Flex flexWrap="wrap">
-            <Box width={[1, 1 / 5]} px={2}>
-              <TopLevelList>
-                {this.topLevelItems.map(item => (
-                  <TopLevelItem key={item.contentful_id}>
-                    <TopLevelItemButton
-                      onClick={this.handleSecondLevel.bind(
-                        this,
-                        item.contentful_id
-                      )}
-                      isActive={secondSelected === item.contentful_id}
+          {topLevelItem ? (
+            <EverythingPageNavigation
+              navigation={this.navigation}
+              topLevelItems={this.topLevelItems}
+              selectedItem={topLevelItem}
+            />
+          ) : (
+            <Flex flexWrap="wrap">
+              {this.topLevelItems.map((item, key) => (
+                <TopLevelBox
+                  key={item.contentful_id}
+                  width={[1, 1, 1 / 4]}
+                  px={2}
+                >
+                  <h4>
+                    <LinkyButton
+                      onClick={event => {
+                        event.preventDefault()
+                        this.setState({
+                          topLevelItem: item.contentful_id,
+                        })
+                      }}
                     >
                       {item.title}
-                    </TopLevelItemButton>
-                  </TopLevelItem>
-                ))}
-              </TopLevelList>
-            </Box>
-            {secondSelected && (
-              <Box width={[1, 2 / 5]} px={2}>
-                <h2>{this.navigation[secondSelected].title}</h2>
-                <SecondLevelList>
-                  {this.navigation[secondSelected].contentfulchildren.map(
-                    id => (
-                      <SecondLevelItem key={id.contentful_id}>
-                        <HiddenButton
-                          onClick={this.handleThirdLevel.bind(
-                            this,
-                            id.contentful_id
-                          )}
-                        >
-                          <h3>{this.navigation[id.contentful_id].title}</h3>
-                          {this.navigation[id.contentful_id]
-                            .childContentfulNavigationItemDescriptionTextNode && (
-                            <p
-                              dangerouslySetInnerHTML={{
-                                __html: this.navigation[id.contentful_id]
-                                  .childContentfulNavigationItemDescriptionTextNode
-                                  .childMarkdownRemark.html,
-                              }}
-                            />
-                          )}
-                        </HiddenButton>
-                      </SecondLevelItem>
-                    )
+                    </LinkyButton>
+                  </h4>
+                  {item.childContentfulNavigationItemDescriptionTextNode && (
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html:
+                          item.childContentfulNavigationItemDescriptionTextNode
+                            .childMarkdownRemark.html,
+                      }}
+                    />
                   )}
-                </SecondLevelList>
-              </Box>
-            )}
-            {thirdSelected && (
-              <Box width={[1, 2 / 5]} px={2}>
-                <h2>{this.navigation[thirdSelected].title}</h2>
-                <ThirdLevelList />
-              </Box>
-            )}
-          </Flex>
+                </TopLevelBox>
+              ))}
+            </Flex>
+          )}
         </Container>
       </Layout>
     )
@@ -172,7 +238,7 @@ export default EverythingPage
 
 export const query = graphql`
   {
-    allContentfulNavigationItem {
+    allContentfulNavigationItem(sort: { fields: [title] }) {
       edges {
         node {
           contentful_id
