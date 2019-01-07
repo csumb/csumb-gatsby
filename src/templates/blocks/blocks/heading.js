17da7236afca967625d66ca5fe7279e59f699408
Kevin Miller <kevin@csumb.edu>
2019-01-07 15:58:34 -0800

    Added style to collapsible header

--- src/templates/blocks/blocks/heading.js
+++ src/templates/blocks/blocks/heading.js
@@ -2,17 +2,34 @@ import React from 'react'
 import styled from 'react-emotion'
 import LinkInspect from 'components/link-inspect'
 import { ContainerContext, ContainerElement } from '../container-context'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+const Chevron = styled(FontAwesomeIcon)`
+  display: inline-block;
+  float: right;
+`
 
 class BlockHeading extends React.Component {
   render() {
     let HeadingTag = styled(`h${this.props.level}`)``
-    const { url, text, uuid } = this.props
+    const { url, text, uuid, collapsible } = this.props
     return (
       <ContainerContext.Consumer>
         {container => (
           <ContainerElement container={container}>
             <HeadingTag id={`heading-${uuid}`}>
-              {url ? <LinkInspect to={url}>{text}</LinkInspect> : <>{text}</>}
+              {collapsible ? (
+                <>
+                  {text}
+                  <Chevron icon={faChevronDown} />
+                </>
+              ) : (
+                  <>
+                    {url ? <LinkInspect to={url}>{text}</LinkInspect> : <>{text}</>}
+                  </>
+                )}
+
             </HeadingTag>
           </ContainerElement>
         )}
