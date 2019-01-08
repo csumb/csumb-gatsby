3113938c2094be44482a79199ac08b726a675cf5
Kevin Miller <kevin@csumb.edu>
2019-01-08 11:19:02 -0800

    Rollback to regular cipher for now

--- src/node/pages.js
+++ src/node/pages.js
@@ -6,7 +6,7 @@ const encryptFeedback = email => {
   if (!email) {
     return null
   }
-  const cipher = crypto.createCipheriv(
+  const cipher = crypto.createCipher(
     'aes-256-ctr',
     process.env.CSUMB_FEEDBACK_KEY
   )
