e358d020116a47c38ce1060ac2cd9af222757454
Kevin Miller <kevin@csumb.edu>
2019-01-07 20:55:17 -0800

    Added toggleable headers, #17

--- src/templates/blocks/blocks/heading.js
+++ src/templates/blocks/blocks/heading.js
@@ -2,17 +2,64 @@ import React from 'react'
 import styled from 'react-emotion'
 import LinkInspect from 'components/link-inspect'
 import { ContainerContext, ContainerElement } from '../container-context'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+
+const CollapsibleIcon = styled(FontAwesomeIcon)`
+  float: right;
+`
+
+const CollapsibleHeading = styled('button')`
+  background: transparent;
+  border: none;
+  padding: none;
+  display: block;
+  width: 100%;
+  text-align: left;
+  cursor: pointer;
+`
 
 class BlockHeading extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  handleToggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    })
+  }
+
   render() {
     let HeadingTag = styled(`h${this.props.level}`)``
-    const { url, text, uuid } = this.props
+    const { url, text, uuid, collapsible, headerHandler } = this.props
+    const { isOpen } = this.state
     return (
       <ContainerContext.Consumer>
         {container => (
           <ContainerElement container={container}>
             <HeadingTag id={`heading-${uuid}`}>
-              {url ? <LinkInspect to={url}>{text}</LinkInspect> : <>{text}</>}
+              {collapsible ? (
+                <CollapsibleHeading
+                  onClick={() => {
+                    headerHandler()
+                    this.handleToggle()
+                  }}
+                >
+                  <CollapsibleIcon
+                    icon={isOpen ? faChevronUp : faChevronDown}
+                  />
+                  {text}
+                </CollapsibleHeading>
+              ) : (
+                <>
+                  {url ? (
+                    <LinkInspect to={url}>{text}</LinkInspect>
+                  ) : (
+                    <>{text}</>
+                  )}
+                </>
+              )}
             </HeadingTag>
           </ContainerElement>
         )}
