6e9137542839d187fd5efead829b15e3db004dfb
Kevin Miller <kevin@csumb.edu>
2019-01-16 14:02:18 -0800

    Fixed missing item

--- src/templates/everything/sub-sub-item.js
+++ src/templates/everything/sub-sub-item.js
@@ -11,6 +11,7 @@ import {
   HiddenLink,
   ThirdLevelList,
   ThirdLevelTitle,
+  EverythingContent
 } from 'components/pages/everything'
 import SiteHeader from 'components/header/site-header'
 import Layout from 'components/layouts/default'
