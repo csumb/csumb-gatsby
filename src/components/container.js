7af83f6a3c839889e0bccfcf223334b5fcd188df
Kevin Miller <kevin@csumb.edu>
2019-01-31 09:07:09 -0800

    Cleaned up container

--- src/components/container.js
+++ src/components/container.js
@@ -2,21 +2,16 @@ import styled from '@emotion/styled'
 import bp from 'components/styles/breakpoints'
 import facepaint from 'facepaint'
 
-const breakpoints = [0, 1110]
-
-const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`))
-
 const Container = styled('div')`
   ${props =>
     props.topPadding &&
     `
     padding-top: 1.5rem;
   `}
-  max-width: 1100px;
-  margin: 0 auto;
-  ${mq({
-      paddingLeft: ['1.5rem', '0'],
-      paddingRight: ['1.5rem', '0'],
+  ${bp({
+      marginLeft: ['1rem', '1rem', 'auto'],
+      marginRight: ['1rem', '1rem', 'auto'],
+      maxWidth: '1100px',
     })}
 `
 
