50b0c5f29ea7ed2fd9716bfcbbf9e1928859608b
Kevin Miller <kevin@csumb.edu>
2019-01-31 09:00:38 -0800

    Fixed container padding

--- src/components/container.js
+++ src/components/container.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import { Box } from '@rebass/grid'
 import styled from '@emotion/styled'
+import bp from 'components/styles/breakpoints'
+import facepaint from 'facepaint'
 
-const ContainerElement = styled(Box)`
+const breakpoints = [0, 1110]
+
+const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`))
+
+const Container = styled('div')`
   ${props =>
     props.topPadding &&
     `
     padding-top: 1.5rem;
-  `};
+  `}
   max-width: 1100px;
+  margin: 0 auto;
+  ${mq({
+      paddingLeft: ['1.5rem', '0'],
+      paddingRight: ['1.5rem', '0'],
+    })}
 `
-
-ContainerElement.defaultProps = {
-  mx: ['1rem', '1rem', 'auto'],
-}
-
-const Container = props => <ContainerElement {...props} />
 
 export default Container
