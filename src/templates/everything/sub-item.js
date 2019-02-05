0f25329004b6b45b5e806f3eba0be88281c137e5
Kevin Miller <kevin@csumb.edu>
2019-02-04 16:09:59 -0800

    Mobile context for everything pages

--- src/templates/everything/sub-item.js
+++ src/templates/everything/sub-item.js
@@ -15,63 +15,88 @@ import SiteHeader from 'components/header/site-header'
 import Link from 'gatsby-link'
 import Layout from 'components/layouts/default'
 import Container from 'components/container'
-import { Flex, Box } from '@rebass/grid/emotion'
-import MediaQuery from 'react-responsive'
+import { Flex, Box } from '@rebass/grid'
 
-const EverythingSubLevelPage = ({ pageContext }) => (
-  <Layout pageTitle="Everything else">
-    <SiteHeader path="/everything">Everything else</SiteHeader>
-    <Container topPadding>
-      <Flex flexWrap="wrap">
-        <MediaQuery query="(max-width: 830px)">
-          <p>
-            <Link to="/everything">← Back to everything</Link>
-          </p>
-        </MediaQuery>
-        <MediaQuery query="(min-width: 830px)">
-          <Box width={[1, 1 / 5]} px={2}>
-            <TopLevelList>
-              {pageContext.topLevelItems.map(item => (
-                <TopLevelItem key={item.contentful_id}>
-                  <TopLevelItemLink
-                    to={`/everything/${item.slug}`}
-                    isActive={
-                      item.contentful_id ===
-                      pageContext.currentItem.contentful_id
-                    }
-                  >
-                    {item.title}
-                  </TopLevelItemLink>
-                </TopLevelItem>
-              ))}
-            </TopLevelList>
-          </Box>
-        </MediaQuery>
-        <Box width={[1, 2 / 5]} px={2}>
-          <SecondLevelTitle>{pageContext.currentItem.title}</SecondLevelTitle>
-          <EverythingContent item={pageContext.currentItem} />
-          <SecondLevelList>
-            {pageContext.currentItems.map(item => (
-              <SubItem key={item.contentful_id}>
-                {!item.link && <SubItemArrow />}
-                <HiddenLink
-                  to={
-                    item.link
-                      ? item.link
-                      : `/everything/${pageContext.currentItem.slug}/${
-                          item.slug
-                        }`
-                  }
-                >
-                  <SubItemContent item={item} />
-                </HiddenLink>
-              </SubItem>
-            ))}
-          </SecondLevelList>
-        </Box>
-      </Flex>
-    </Container>
-  </Layout>
-)
+class EverythingSubLevelPage extends React.Component {
+  state = {
+    isMobile: false,
+  }
+
+  componentDidMount() {
+    const mobileBreakpoint = 830
+    const that = this
+
+    const setWindowSize = () => {
+      that.setState({
+        isMobile: window.innerWidth <= mobileBreakpoint,
+      })
+    }
+
+    window.addEventListener('resize', setWindowSize)
+
+    setWindowSize()
+  }
+
+  render() {
+    const { pageContext } = this.props
+    const { isMobile } = this.state
+    return (
+      <Layout pageTitle="Everything else">
+        <SiteHeader path="/everything">Everything else</SiteHeader>
+        <Container topPadding>
+          <Flex flexWrap="wrap">
+            {isMobile ? (
+              <p>
+                <Link to="/everything">← Back to everything</Link>
+              </p>
+            ) : (
+              <Box width={[1, 1 / 5]} px={2}>
+                <TopLevelList>
+                  {pageContext.topLevelItems.map(item => (
+                    <TopLevelItem key={item.contentful_id}>
+                      <TopLevelItemLink
+                        to={`/everything/${item.slug}`}
+                        isActive={
+                          item.contentful_id ===
+                          pageContext.currentItem.contentful_id
+                        }
+                      >
+                        {item.title}
+                      </TopLevelItemLink>
+                    </TopLevelItem>
+                  ))}
+                </TopLevelList>
+              </Box>
+            )}
+            <Box width={[1, 2 / 5]} px={2}>
+              <SecondLevelTitle>
+                {pageContext.currentItem.title}
+              </SecondLevelTitle>
+              <EverythingContent item={pageContext.currentItem} />
+              <SecondLevelList>
+                {pageContext.currentItems.map(item => (
+                  <SubItem key={item.contentful_id}>
+                    {!item.link && <SubItemArrow />}
+                    <HiddenLink
+                      to={
+                        item.link
+                          ? item.link
+                          : `/everything/${pageContext.currentItem.slug}/${
+                              item.slug
+                            }`
+                      }
+                    >
+                      <SubItemContent item={item} />
+                    </HiddenLink>
+                  </SubItem>
+                ))}
+              </SecondLevelList>
+            </Box>
+          </Flex>
+        </Container>
+      </Layout>
+    )
+  }
+}
 
 export default EverythingSubLevelPage
