0533da2dab99e820a262922cf935e87709d3c253
Kevin Miller <kevin@csumb.edu>
2019-02-05 12:01:32 -0800

    Fixed image grids

--- src/templates/blocks/blocks/image-grid.js
+++ src/templates/blocks/blocks/image-grid.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import LinkInspect from 'components/link-inspect'
-import { ContainerElement } from '../container-context'
+import { ContainerContext, ContainerElement } from '../container-context'
 import { Flex, Box } from '@rebass/grid/emotion'
 
 const ImageGridHeader = styled('h3')`
@@ -16,19 +16,28 @@ class BlockImageGrid extends React.Component {
   render() {
     const { images, columnWidth } = this.props
     return (
-      <ContainerElement isFull>
-        <Flex flexWrap="wrap">
-          {images.map(image => (
-            <Box width={[1, 1, columnWidth / 12, columnWidth / 12]} px={2}>
-              <LinkInspect to={image.link}>
-                <ImageGridImage src={image.image.url} alt="" />
-                <ImageGridHeader>{image.headline}</ImageGridHeader>
-              </LinkInspect>
-              {image.text && <p>{image.text}</p>}
-            </Box>
-          ))}
-        </Flex>
-      </ContainerElement>
+      <ContainerContext.Consumer>
+        {container => (
+          <ContainerElement container={container}>
+            <Flex flexWrap="wrap">
+              {images.map(image => (
+                <Box width={[1, 1, columnWidth / 12, columnWidth / 12]} px={2}>
+                  <LinkInspect to={image.link}>
+                    <ImageGridImage
+                      src={`https://s3.amazonaws.com/csumb-uploads/${
+                        image.image.key
+                      }`}
+                      alt=""
+                    />
+                    <ImageGridHeader>{image.headline}</ImageGridHeader>
+                  </LinkInspect>
+                  {image.text && <p>{image.text}</p>}
+                </Box>
+              ))}
+            </Flex>
+          </ContainerElement>
+        )}
+      </ContainerContext.Consumer>
     )
   }
 }
