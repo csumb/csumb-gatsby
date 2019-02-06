5d79e370e9ee9c80ea784fb19c6cd7d947872c1d
Kevin Miller <kevin@csumb.edu>
2019-02-06 13:30:54 -0800

    Fixed everything else links in 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,27 +9,42 @@ import { Flex, Box } from '@rebass/grid/emotion'
 import EventsSidebar from 'components/events-sidebar'
 import SiteHeader from 'components/header/site-header'
 import Link from 'gatsby-link'
+import moment from 'moment'
+import { AlertFyi } from 'components/alert'
 
-const ErrorEventPage = () => (
-  <>
-    <SiteHeader path="/events">Events</SiteHeader>
-    <Container topPadding>
-      <PageTitle>Events for </PageTitle>
-      <Flex flexWrap="wrap">
-        <Box width={[1, 3 / 4, 3 / 4]} pr={[0, 4, 4]} />
-        <Box width={[1, 1 / 4, 1 / 4]}>
-          <EventsSidebar />
-        </Box>
-      </Flex>
-    </Container>
-  </>
-)
+const ErrorEventPage = () => {
+  const dateParts = window.location.pathname.replace('/events/', '').split('/')
+  const dateFormat = moment()
+    .set({
+      year: dateParts[0],
+      month: parseInt(dateParts[1]) - 1,
+      date: dateParts[2],
+    })
+    .format('MMMM D, YYYY')
+  return (
+    <>
+      <SiteHeader path="/events">Events</SiteHeader>
+      <Container topPadding>
+        <PageTitle>Events for {dateFormat}</PageTitle>
+        <Flex flexWrap="wrap">
+          <Box width={[1, 3 / 4, 3 / 4]} pr={[0, 4, 4]}>
+            <AlertFyi>Sorry, there are no events on {dateFormat}.</AlertFyi>
+          </Box>
+          <Box width={[1, 1 / 4, 1 / 4]}>
+            <EventsSidebar />
+          </Box>
+        </Flex>
+      </Container>
+    </>
+  )
+}
 
 const ErrorPage = props => {
   const items = props.data.allContentfulNavigationItem.edges
   const isEvent =
     typeof window !== 'undefined' &&
     window.location.href.search('/events/') > -1
+
   return (
     <Layout pageTitle="Page not found">
       {isEvent ? (
@@ -43,7 +58,7 @@ const ErrorPage = props => {
             you're looking for:
           </LeadParagraph>
           <Flex flexWrap="wrap">
-            {items.forEach(edge => (
+            {items.map(edge => (
               <>
                 {edge.node.topLevelItem && (
                   <TopLevelBox
