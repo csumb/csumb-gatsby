cfabe80dade21c91401876034a45d79f25410f18
Kevin Miller <kevin@csumb.edu>
2019-02-18 14:24:14 -0800

    Added filter to dean's list

--- src/pages/academics/deans-list.js
+++ src/pages/academics/deans-list.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from 'components/layouts/default'
 import { graphql } from 'gatsby'
 import { InputText, Submit } from 'components/forms'
+import Well from 'components/well'
 import Container from 'components/container'
 import SiteHeader from 'components/header/site-header'
 import PageTitle from 'components/header/page-title'
@@ -10,11 +11,20 @@ import { Table, TableRow, TableHeader, TableCell } from 'components/table'
 
 class AcademicsPage extends React.Component {
   state = {
-    filter: false,
+    query: false,
+    search: false,
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({
+      search: this.state.query.length > 0 ? this.state.query : false,
+    })
   }
 
   render() {
     const { data } = this.props
+    const { search } = this.state
     return (
       <Layout pageTitle="Academics">
         <SiteHeader path="/academics">Academics</SiteHeader>
@@ -32,6 +42,23 @@ class AcademicsPage extends React.Component {
             minimum of 12 letter-graded units with no incomplete grades, no
             grade lower than "C" and be in good standing.
           </p>
+          <Well>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <InputText
+                name="search"
+                label="Search dean's list"
+                hideLabel={true}
+                placeholder="Search by name"
+                inline
+                onChange={event => {
+                  this.setState({
+                    query: event.target.value,
+                  })
+                }}
+              />
+              <Submit value="Search" small style={{ marginLeft: '1rem' }} />
+            </form>
+          </Well>
           <Table>
             <thead>
               <tr>
@@ -42,13 +69,20 @@ class AcademicsPage extends React.Component {
               </tr>
             </thead>
             <tbody>
-              {data.allDeansListCsv.edges.map(node => (
-                <TableRow>
-                  <TableCell>{node.node.college}</TableCell>
-                  <TableCell>{node.node.major}</TableCell>
-                  <TableCell>{node.node.last_name}</TableCell>
-                  <TableCell>{node.node.first_name}</TableCell>
-                </TableRow>
+              {data.allDeansListCsv.edges.map((node, index) => (
+                <React.Fragment key={index}>
+                  {(!search ||
+                    `${node.node.first_name} ${node.node.last_name}`
+                      .toLowerCase()
+                      .search(search.toLowerCase()) > -1) && (
+                    <TableRow>
+                      <TableCell>{node.node.college}</TableCell>
+                      <TableCell>{node.node.major}</TableCell>
+                      <TableCell>{node.node.last_name}</TableCell>
+                      <TableCell>{node.node.first_name}</TableCell>
+                    </TableRow>
+                  )}
+                </React.Fragment>
               ))}
             </tbody>
           </Table>
