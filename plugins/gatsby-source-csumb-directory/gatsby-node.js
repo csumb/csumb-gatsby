55820c26ef3c24f6eafbe0e561eaab92d3fcff82
Kevin Miller <kevin@csumb.edu>
2019-02-18 13:11:16 -0800

    Change directory to single node type

---
+++ plugins/gatsby-source-csumb-directory/gatsby-node.js
@@ -0,0 +1,42 @@
+const walk = require('walk')
+const fs = require('fs-extra')
+const crypto = require('crypto')
+
+exports.sourceNodes = async ({ actions, createNodeId }, configOptions) => {
+  const { createNode } = actions
+  const allUsers = {}
+  const allDirectory = fs.readJSONSync('./_web-content/_data/directory.json')
+  allDirectory.forEach(user => {
+    allUsers[user.login.split('@').shift()] = user
+  })
+
+  walk.walkSync('./_web-content/_data/public-directory', {
+    listeners: {
+      file: async (root, fileStats, next) => {
+        const fileName = `${root}/${fileStats.name}`
+        const login = fileStats.name.replace('.json', '')
+        if (typeof allUsers[login] !== 'undefined') {
+          const contents = await fs.readJson(fileName)
+          allUsers[login]._publicProfile = contents
+        }
+      },
+    },
+  })
+
+  Object.keys(allUsers).forEach(id => {
+    const directoryNode = {
+      id: createNodeId(`${id} >>> CsumbDirectory`),
+      children: [],
+      parent: null,
+      user: allUsers[id],
+      internal: {
+        type: `CsumbDirectory`,
+      },
+    }
+    directoryNode.internal.contentDigest = crypto
+      .createHash(`md5`)
+      .update(JSON.stringify(directoryNode))
+      .digest(`hex`)
+    createNode(directoryNode)
+  })
+}
