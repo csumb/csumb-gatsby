8fbf029564580030edd75e3a6032cf76f1826ca2
Kevin Miller <kevin@csumb.edu>
2019-02-18 15:10:25 -0800

    Added fetch polyfill.io check

---
+++ gatsby-browser.js
@@ -0,0 +1,19 @@
+exports.onClientEntry = () => {
+  return new Promise((resolve, reject) => {
+    window.__polyfillio__ = () => {
+      resolve()
+    }
+
+    const features = []
+
+    if (!('fetch' in window)) {
+      const s = document.createElement('script')
+      s.src = `https://cdn.polyfill.io/v2/polyfill.min.js?features=${features.join(
+        ','
+      )}&rum=1&flags=always&callback=__polyfillio__`
+      s.async = true
+      s.onerror = reject
+      document.head.appendChild(s)
+    }
+  })
+}
