8ea78e1eddf8691400184739e5e0b9f82455f4c9
Kevin Miller <kevin@csumb.edu>
2019-02-25 10:54:45 -0800

    Fixed jest preprocess

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '^components/(.*)': '<rootDir>/src/components/$1',
+    '^style/(.*)': '<rootDir>/src/style/$1',
   },
   testPathIgnorePatterns: ['node_modules', '.cache', 'src/custom-build'],
   transformIgnorePatterns: [
