a17e21a3eb0b6b5e137c2f679364d32c6205df47
Kevin Miller <kevin@csumb.edu>
2019-03-01 15:48:11 -0800

    Went back to dangous for text blocks

--- src/templates/blocks/blocks/text.js
+++ src/templates/blocks/blocks/text.js
@@ -2,9 +2,6 @@ import React from 'react'
 import filterCourses from './filter-courses'
 import styled from '@emotion/styled'
 import { ContainerContext, ContainerElement } from '../container-context'
-import LinkInspect from 'components/link-inspect'
-import parse from 'html-react-parser'
-import domToReact from 'html-react-parser/lib/dom-to-react'
 
 const Paragraph = styled('p')`
   ${props =>
@@ -15,18 +12,6 @@ const Paragraph = styled('p')`
       : ``};
 `
 
-const parserOptions = {
-  replace: ({ type, name, attribs, children }) => {
-    if (type === 'tag' && name === 'a') {
-      return (
-        <LinkInspect to={attribs.href}>
-          {domToReact(children, parserOptions)}
-        </LinkInspect>
-      )
-    }
-  },
-}
-
 const BlockText = ({ lead, text }) => {
   if (!text) {
     return null
@@ -35,9 +20,12 @@ const BlockText = ({ lead, text }) => {
     <ContainerContext.Consumer>
       {container => (
         <ContainerElement container={container}>
-          <Paragraph lead={lead} container={container}>
-            {parse(filterCourses(text), parserOptions)}
-          </Paragraph>
+          <Paragraph
+            lead={lead}
+            dangerouslySetInnerHTML={{
+              __html: filterCourses(text),
+            }}
+          />
         </ContainerElement>
       )}
     </ContainerContext.Consumer>
