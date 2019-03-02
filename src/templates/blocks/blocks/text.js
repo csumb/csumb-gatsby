e23a45cd451c7314d9d9eeb71d222228404ca36b
Kevin Miller <kevin@csumb.edu>
2019-03-01 16:08:12 -0800

    Problem wasn't text blocks, actually!

--- src/templates/blocks/blocks/text.js
+++ src/templates/blocks/blocks/text.js
@@ -2,6 +2,9 @@ import React from 'react'
 import filterCourses from './filter-courses'
 import styled from '@emotion/styled'
 import { ContainerContext, ContainerElement } from '../container-context'
+import LinkInspect from 'components/link-inspect'
+import parse from 'html-react-parser'
+import domToReact from 'html-react-parser/lib/dom-to-react'
 
 const Paragraph = styled('p')`
   ${props =>
@@ -12,6 +15,18 @@ const Paragraph = styled('p')`
       : ``};
 `
 
+const parserOptions = {
+  replace: ({ type, name, attribs, children }) => {
+    if (type === 'tag' && name === 'a') {
+      return (
+        <LinkInspect to={attribs.href}>
+          {domToReact(children, parserOptions)}
+        </LinkInspect>
+      )
+    }
+  },
+}
+
 const BlockText = ({ lead, text }) => {
   if (!text) {
     return null
@@ -20,12 +35,9 @@ const BlockText = ({ lead, text }) => {
     <ContainerContext.Consumer>
       {container => (
         <ContainerElement container={container}>
-          <Paragraph
-            lead={lead}
-            dangerouslySetInnerHTML={{
-              __html: filterCourses(text),
-            }}
-          />
+          <Paragraph lead={lead} container={container}>
+            {parse(filterCourses(text), parserOptions)}
+          </Paragraph>
         </ContainerElement>
       )}
     </ContainerContext.Consumer>
