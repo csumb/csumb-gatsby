606090cbb9f9caf1833dfd01cf4da7a295d4551e
Kevin Miller <kevin@csumb.edu>
2019-03-06 15:04:59 -0800

    Blockquote & hero image styling

--- src/templates/blocks/blocks/quote.js
+++ src/templates/blocks/blocks/quote.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import LinkInspect from 'components/link-inspect'
-import { ContainerElement, containerStyle } from '../container-context'
+import { ContainerElement, ContainerContext } from '../container-context'
+import bp from 'style/breakpoints'
 
 const Cite = styled('cite')`
   display: block;
@@ -12,28 +13,37 @@ const Quote = styled('div')`
   ${props => props.container};
 `
 
-const Blockquote = styled('blockquote')``
+const Blockquote = styled('blockquote')`
+  font-size: 1.3rem;
+  ${bp({
+    padding: ['0 0.5rem', '0 1rem', '0 3.5rem'],
+  })}
+`
 
 class BlockQuote extends React.Component {
   render() {
     const { quote, source, url } = this.props
     return (
-      <ContainerElement container={containerStyle.slightlyLarger}>
-        <Quote>
-          <Blockquote>
-            {quote}
-            {source && (
-              <Cite>
-                {url ? (
-                  <LinkInspect to={url}>{source}</LinkInspect>
-                ) : (
-                  <>{source}</>
+      <ContainerContext.Consumer>
+        {container => (
+          <ContainerElement container={container}>
+            <Quote>
+              <Blockquote>
+                {quote}
+                {source && (
+                  <Cite>
+                    {url ? (
+                      <LinkInspect to={url}>{source}</LinkInspect>
+                    ) : (
+                      <>{source}</>
+                    )}
+                  </Cite>
                 )}
-              </Cite>
-            )}
-          </Blockquote>
-        </Quote>
-      </ContainerElement>
+              </Blockquote>
+            </Quote>
+          </ContainerElement>
+        )}
+      </ContainerContext.Consumer>
     )
   }
 }
