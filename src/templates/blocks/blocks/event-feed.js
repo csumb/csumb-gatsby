99cea27c99e8ff835adb818c47f04ac9867a95cf
Kevin Miller <kevin@csumb.edu>
2019-03-21 17:05:48 -0300

    Change SSR to exist but not do anything

--- src/templates/blocks/blocks/event-feed.js
+++ src/templates/blocks/blocks/event-feed.js
@@ -24,15 +24,18 @@ const displayEvent = item => {
 class BlockEventFeed extends React.Component {
   render() {
     const { events, title, limit } = this.props
+    let count = 0
+    const displayEvents = []
+    events.map(event => {
+      if (displayEvent(event) && displayEvents.length < limit) {
+        displayEvents.push(event)
+      }
+    })
     return (
       <EventFeedWrapper>
         {title && <h3>{title}</h3>}
-        {events.map((event, index) => (
-          <React.Fragment key={index}>
-            {displayEvent(event) && index < limit && (
-              <EventFeedItem {...event} key={index} />
-            )}
-          </React.Fragment>
+        {displayEvents.map((event, index) => (
+          <EventFeedItem {...event} key={index} />
         ))}
       </EventFeedWrapper>
     )
