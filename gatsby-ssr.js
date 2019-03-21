a17d6f3df5d992066b4a4da9eeeb0321a836524b
Kevin Miller <kevin@csumb.edu>
2019-03-21 17:05:48 -0300

    Change SSR to exist but not do anything

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,34 +2,9 @@ import React from 'react'
 import { UserContext, setUserRole } from 'components/contexts/user'
 
 class UserComponent extends React.Component {
-  state = {
-    user: false,
-  }
-
-  async componentDidMount() {
-    window
-      .fetch('https://csumb.okta.com/api/v1/users/me', {
-        credentials: 'include',
-      })
-      .then(response => {
-        return response.json()
-      })
-      .then(user => {
-        user = setUserRole(user)
-        this.setState({
-          user: user,
-        })
-      })
-      .catch(error => {
-        this.setState({
-          user: { anonymous: true },
-        })
-      })
-  }
-
   render() {
     return (
-      <UserContext.Provider value={{ user: this.state.user }}>
+      <UserContext.Provider value={{ user: false }}>
         {this.props.children}
       </UserContext.Provider>
     )
