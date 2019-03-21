e27eef925fd7614959dff78058e7126bee86c68a
Kevin Miller <kevin@csumb.edu>
2019-03-21 14:46:18 -0300

    Moved user info to a bigger scope

---
+++ gatsby-browser.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { UserContext, setUserRole } from 'components/contexts/user'
+
+class UserComponent extends React.Component {
+  state = {
+    user: false,
+  }
+
+  async componentDidMount() {
+    window
+      .fetch('https://csumb.okta.com/api/v1/users/me', {
+        credentials: 'include',
+      })
+      .then(response => {
+        return response.json()
+      })
+      .then(user => {
+        user = setUserRole(user)
+        this.setState({
+          user: user,
+        })
+      })
+      .catch(error => {
+        this.setState({
+          user: { anonymous: true },
+        })
+      })
+  }
+
+  render() {
+    return (
+      <UserContext.Provider value={{ user: this.state.user }}>
+        {this.props.children}
+      </UserContext.Provider>
+    )
+  }
+}
+
+export const wrapPageElement = ({ element, props }) => {
+  return <UserComponent {...props}>{element}</UserComponent>
+}
