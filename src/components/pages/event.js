a7a25039a713ea7937fe6ff290502164129956a0
Kevin Miller <kevin@csumb.edu>
2019-03-26 16:25:32 -0300

    Event address fix

--- src/components/pages/event.js
+++ src/components/pages/event.js
@@ -80,10 +80,22 @@ const Event = ({ event }) => (
             )}
             {event.location.type === 'off-campus' && (
               <address>
-                {event.location.address.organisation}
+                {event.location.address.organisation && (
+                  <>
+                    {event.location.address.organisation}
+                    <br />
+                  </>
+                )}
                 {event.location.address.street}
-                {event.location.address.premise}
-                {event.location.address.city}, {event.location.address.state}{' '}
+                <br />
+                {event.location.address.premise && (
+                  <>
+                    event.location.address.premise}
+                    <br />
+                  </>
+                )}
+                {event.location.address.city}, {event.location.address.state}
+                {'  '}
                 {event.location.address.zip}
               </address>
             )}
