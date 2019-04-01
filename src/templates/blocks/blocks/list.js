da54d00e69e3870ff2bdc7f948dc38365ee12875
Kevin Miller <kevin@csumb.edu>
2019-04-01 19:56:02 -0300

    Fixed list indenting

--- src/templates/blocks/blocks/list.js
+++ src/templates/blocks/blocks/list.js
@@ -13,14 +13,62 @@ class BlockList extends React.Component {
             margin-left: 0;
           `
         : styled(type)``
+
+    const listItems = []
+    let priorLevel = 1
+    let firstLevel = 0
+    let secondLevel = 0
+    list.forEach((item, key) => {
+      if (
+        !item.text ||
+        item.text.length === 0 ||
+        item.text.trim().length === 0
+      ) {
+        return
+      }
+      if (priorLevel === 1 && item.level === 3) {
+        item.level = 2
+      }
+      priorLevel = item.level
+      if (item.level === 1) {
+        item._children = []
+        listItems[key] = item
+        firstLevel = key
+        return
+      }
+      if (item.level === 2) {
+        item._children = []
+        listItems[firstLevel]._children[key] = item
+        secondLevel = key
+        return
+      }
+
+      if (item.level === 3) {
+        listItems[firstLevel]._children[secondLevel]._children[key] = item
+      }
+    })
     return (
       <ListTag>
-        {list.map((item, key) => (
-          <React.Fragment key={key}>
-            {item.text && item.text.trim().length > 0 && (
-              <li>{parseHtml(item.text)}</li>
+        {listItems.map((item, key) => (
+          <li>
+            {parseHtml(item.text)}
+            {item._children.length && (
+              <ListTag>
+                {item._children.map((secondItem, secondKey) => (
+                  <li>
+                    {parseHtml(secondItem.text)}
+                    {secondItem._children.length && (
+                      <ListTag>
+                        {secondItem._children.map(thirdItem => (
+                          <li>{parseHtml(thirdItem.text)}</li>
+                        ))}
+                      </ListTag>
+                    )}
+                  </li>
+                ))}
+              </ListTag>
             )}
-          </React.Fragment>
+          </li>
         ))}
       </ListTag>
     )
