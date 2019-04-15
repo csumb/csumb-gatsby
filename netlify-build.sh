b70e6a9b97fbdbf7d8f300391be5e49d0047dd0d
Kevin Miller <kevin@csumb.edu>
2019-04-15 10:47:07 -0300

    Added build step

---
+++ netlify-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone --depth 1 https://$GITHUB_TOKEN@github.com/csumb/web-data.git _data
+git clone --depth 1 https://$GITHUB_TOKEN@github.com/csumb/web-content.git _web-content
+npm run test
+node --max_old_space_size=12000 ./node_modules/.bin/gatsby build
+npm run directoryJson
+npm run scheduleJson
+npm run catalogJson
+npm run eventJson
+npm run buildJson
+npm run configFirebase
+npm run canary
