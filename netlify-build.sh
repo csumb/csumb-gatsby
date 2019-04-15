a9ebbcb6671a905c86610081741f70cb94de8df1
Kevin Miller <kevin@csumb.edu>
2019-04-15 11:02:51 -0300

    Removed test suites

--- netlify-build.sh
+++ netlify-build.sh
@@ -2,7 +2,6 @@
 
 git clone --depth 1 https://$GITHUB_TOKEN@github.com/csumb/web-data.git _data
 git clone --depth 1 https://$GITHUB_TOKEN@github.com/csumb/web-content.git _web-content
-npm run test
 node --max_old_space_size=12000 ./node_modules/.bin/gatsby build
 npm run directoryJson
 npm run scheduleJson
