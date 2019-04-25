7bfc39e43165cfb77aba249d18edfb069ada8a47
Kevin Miller <kevin@csumb.edu>
2019-04-25 16:50:52 -0300

    Added page feedback

---
+++ functions/lib/feedback/index.js
@@ -0,0 +1,26 @@
+const cryptex = require('node-cryptex')
+const feedbackTemplate = require('./feedback')
+const functions = require('firebase-functions')
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(functions.config().sendgrid.key)
+
+module.export = (request, response) => {
+  const iv = new Buffer(16)
+  const feedbackEmail = cryptex.decrypt(
+    request.query.feedbackEmail,
+    functions.config().feedback.secret,
+    iv
+  )
+
+  const feedbackHTML = feedbackTemplate(request.query)
+  const msg = {
+    to: feedbackEmail,
+    from: 'webservices@csumb.edu',
+    subject: `Page feedback on ${request.query.title}`,
+    html: feedbackHTML,
+  }
+  sgMail.send(msg)
+  response.send(JSON.stringify({ success: true }))
+  response.end()
+}
