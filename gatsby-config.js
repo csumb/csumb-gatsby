bd5b7011d2e670138ea7204ea8ead12ba89d6dbc
Kevin Miller <kevin@csumb.edu>
2019-05-01 16:19:55 -0300

    Added client-side redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,7 @@ const gatsbyConfig = {
     'gatsby-plugin-catch-links',
     'gatsby-source-csumb-directory',
     'gatsby-source-csumb-web-content',
+    'gatsby-plugin-client-side-redirect',
     {
       resolve: 'gatsby-plugin-nprogress',
       options: {
---
+++ src/node/redirects.js
@@ -0,0 +1,35 @@
+module.exports = (graphql, actions) => {
+  const { createRedirect } = actions
+  return new Promise((resolve, reject) => {
+    resolve(
+      graphql(`
+        {
+          allCsumbRedirect {
+            edges {
+              node {
+                redirect {
+                  source
+                  target
+                }
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+          return
+        }
+        result.data.allCsumbRedirect.edges.forEach(({ node }) => {
+          createRedirect({
+            fromPath: node.redirect.source,
+            toPath: node.redirect.target,
+            isPermanent: true,
+          })
+        })
+
+        return
+      })
+    )
+  })
+}
