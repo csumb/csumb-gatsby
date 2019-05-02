96d94ed261649492ee99b8d27ee11d783931e5f3
Kevin Miller <kevin@csumb.edu>
2019-05-02 15:54:08 -0300

    Login message

--- src/components/dashboard/not-logged-in.js
+++ src/components/dashboard/not-logged-in.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import Loading from 'components/common/loading'
+import { ButtonLink } from 'components/common/button'
 
 const loginUrl =
   'https://csumb.okta.com/home/csumb_csumbbetawebsite_1/0oalhdw605Fe37hnQ0x7/alnlhdyx6zseWNBdS0x7'
@@ -11,33 +11,15 @@ const NotLoggedIn = styled('div')`
   margin: 1rem 0;
 `
 
-const LoginMessage = styled('p')`
-  font-size: 0.8rem;
-  margin: 0.5rem 0;
-`
-
-class DashboardNotLoggedIn extends React.Component {
-  componentDidMount() {
-    setTimeout(() => {
-      window.location.href = loginUrl
-    }, 3000)
-  }
-
-  render() {
-    return (
-      <>
-        <NotLoggedIn>
-          <p>
-            <Loading />
-            Logging you in...
-          </p>
-        </NotLoggedIn>
-        <LoginMessage style={{ fontSize: '0.8rem;' }}>
-          Not working? <a href={loginUrl}>Login here</a>
-        </LoginMessage>
-      </>
-    )
-  }
-}
+const DashboardNotLoggedIn = () => (
+  <>
+    <NotLoggedIn>
+      <p>You aren't logged in</p>
+      <ButtonLink to={loginUrl} huge>
+        Log in
+      </ButtonLink>
+    </NotLoggedIn>
+  </>
+)
 
 export default DashboardNotLoggedIn
