eeb6a8f529f4369629c4b80d230ec2356bbd048c
Kevin Miller <kevin@csumb.edu>
2019-05-06 12:39:30 -0300

    Add links to buildings on map #156

--- src/pages/about/map-directions.js
+++ src/pages/about/map-directions.js
@@ -3,7 +3,7 @@ import Layout from 'components/layouts/default'
 import SiteHeader from 'components/layouts/sections/header/site-header'
 import Blocks from 'templates/blocks'
 import styled from '@emotion/styled'
-import { Map, GoogleApiWrapper, InfoWindow } from 'google-maps-react'
+import { Map, GoogleApiWrapper } from 'google-maps-react'
 import { graphql } from 'gatsby'
 import { colors } from 'style/theme'
 import Link from 'gatsby-link'
@@ -16,42 +16,50 @@ const MapMap = styled(Map)`
   margin-bottom: 1.5rem;
 `
 
-class MapPage extends React.Component {
-  state = {
-    currentBuilding: false,
-  }
+const BuildingLink = styled(Link)`
+  font-size: 1rem;
+  font-weight: bold;
+`
 
+class MapPage extends React.Component {
   onReady(mapProps, map) {
+    const infoWindow = new this.props.google.maps.InfoWindow({ maxWidth: 320 })
     map.data.setStyle({
       fillColor: colors.primary.dark,
       fillOpacity: 0.9,
       strokeWeight: 0,
     })
-    this.props.data.allCsumbBuilding.edges.forEach(edge => {
+    this.props.data.allCsumbBuilding.edges.forEach(({ node }) => {
       map.data.addGeoJson({
         type: 'FeatureCollection',
         features: [
           {
             type: 'Feature',
             properties: {
-              name: edge.node.buildingName,
-              code: edge.node.code,
+              name: node.buildingName,
+              code: node.code,
+              center: node.center,
             },
-            geometry: edge.node.outline,
+            geometry: node.outline,
           },
         ],
       })
-      map.data.addListener('click', event => {
-        this.setState({
-          currentBuilding: event.feature.getProperty('code'),
-        })
-      })
+    })
+
+    map.data.addListener('click', event => {
+      const code = event.feature.getProperty('code')
+      const name = event.feature.getProperty('name')
+      const center = event.feature.getProperty('center')
+      infoWindow.setContent(`<a style="font-weight: bold; font-size: 120%; display: inline-block; padding: 0.5rem;" href="/directory/building/${code}">
+      ${name}
+    </a>`)
+      infoWindow.setPosition(center)
+      infoWindow.open(map)
     })
   }
 
   render() {
     const { google, data } = this.props
-    const { currentBuilding } = this.state
     return (
       <Layout>
         <SiteHeader path="/map">Map</SiteHeader>
@@ -63,16 +71,7 @@ class MapPage extends React.Component {
             lng: -121.7989176,
           }}
           onReady={this.onReady.bind(this)}
-        >
-          {this.props.data.allCsumbBuilding.edges.map(({ node }) => (
-            <InfoWindow
-              position={node.center}
-              visible={node.code === currentBuilding}
-            >
-              <a href={`/building/${node.code}`}>{node.buildingName}</a>
-            </InfoWindow>
-          ))}
-        </MapMap>
+        />
         {data.allCsumbPage && (
           <Blocks blocks={data.allCsumbPage.edges[0].node.pageContent} />
         )}
