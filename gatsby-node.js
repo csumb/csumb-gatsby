f3e41a95bfcbfb25b6a347929bf8a4b1618972c7
Kevin Miller <kevin@csumb.edu>
2019-05-17 12:48:26 -0300

    Remove course buildings step for single course view endpoint

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,4 @@
 const path = require(`path`)
-const fs = require(`fs-extra`)
-const coursePages = require(`./src/node/courses`)
 const redirects = require('./src/node/redirects')
 const contentPages = require(`./src/node/pages`)
 const schedulePages = require(`./src/node/schedule`)
@@ -16,10 +14,7 @@ require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ stage, graphql, actions }) => {
   return new Promise((resolve, reject) => {
-    coursePages(graphql, actions)
-      .then(() => {
-        return newsStoryPages(graphql, actions)
-      })
+    newsStoryPages(graphql, actions)
       .then(() => {
         return scienceIllustrationPages(graphql, actions)
       })
