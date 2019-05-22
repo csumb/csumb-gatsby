cfd8d15f57e87feaa6bdcdf571451603a1a1ba2e
Kevin Miller <kevin@csumb.edu>
2019-05-22 12:54:36 -0300

    Added redirect support

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,12 @@ const gatsbyConfig = {
     'gatsby-source-csumb-directory',
     'gatsby-source-csumb-web-content',
     {
+      resolve: 'gatsby-source-csumb-content-api-redirects',
+      options: {
+        endpoint: process.env.CSUMB_CONTENT_API_ENDPOINT,
+      },
+    },
+    {
       resolve: 'gatsby-source-csumb-content-api-sites',
       options: {
         endpoint: process.env.CSUMB_CONTENT_API_ENDPOINT,
---
+++ plugins/gatsby-source-csumb-content-api-redirects/gatsby-node.js
@@ -0,0 +1,41 @@
+const crypto = require('crypto')
+const fetch = require('node-fetch')
+const request = require('sync-request')
+
+exports.sourceNodes = async (
+  { actions, createNodeId, reporter },
+  configOptions
+) => {
+  const { createNode } = actions
+  const { endpoint } = configOptions
+
+  fetchActivity = reporter.activityTimer(
+    'Downloading redirects from CSUMB editor'
+  )
+  fetchActivity.start()
+
+  const response = request('GET', `${endpoint}&type=redirects`)
+  const redirects = JSON.parse(response.getBody())
+
+  Object.keys(redirects.redirects).forEach(source => {
+    let redirectNode = {
+      id: createNodeId(`${source} >>> CsumbRedirect`),
+      children: [],
+      redirect: {
+        source: source,
+        target: redirects.redirects[source],
+      },
+      parent: null,
+      internal: {
+        type: `CsumbRedirect`,
+      },
+    }
+    redirectNode.internal.contentDigest = crypto
+      .createHash(`md5`)
+      .update(JSON.stringify(redirectNode))
+      .digest(`hex`)
+    createNode(redirectNode)
+  })
+
+  fetchActivity.end()
+}
