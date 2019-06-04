a65e2710e5a2f36022bcc154bbc514d195231d9a
Kevin Miller <kevin@csumb.edu>
2019-06-04 16:35:33 -0300

    Move lookup to FB function #328

--- src/pages/account/lookup.js
+++ src/pages/account/lookup.js
@@ -59,7 +59,7 @@ class LookupForm extends Component {
     const { firstName, lastName, dob } = this.state
     event.preventDefault()
     fetch(
-      `https://api.csumb.edu/okta/lookup?firstName=${firstName}&lastName=${lastName}&dob=${dob}`
+      `/cloud-functions/okta/lookup?firstName=${firstName}&lastName=${lastName}&dob=${dob}`
     )
       .then(response => {
         return response.json()
