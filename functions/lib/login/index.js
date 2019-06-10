49627e1c78e1477cf3ac4536da33da4b268c94db
Kevin Miller <kevin@csumb.edu>
2019-06-10 09:39:04 -0300

    Added session check to login

--- functions/lib/login/index.js
+++ functions/lib/login/index.js
@@ -1,10 +1,11 @@
 const saml = require('samlify')
 const metadata = require('./metadata')
 const functions = require('firebase-functions')
+const bcrypt = require('bcrypt')
 
 const hosts = {
   local: {
-    domain: 'localhost:8000',
+    domain: 'localhost:5000',
     redirect: 'http://localhost:8000/dashboard',
     secure: '',
   },
@@ -19,30 +20,47 @@ const hosts = {
     secure: '; Secure',
   },
 }
-
-const host = hosts[functions.config().login.metadata]
+const { instance, salt } = functions.config().login
+const host = hosts[instance]
 
 const idp = saml.IdentityProvider({
-  metadata: metadata.idp[functions.config().login.metadata],
+  metadata: metadata.idp[instance],
 })
 
 const sp = saml.ServiceProvider({
-  metadata: metadata.sp[functions.config().login.metadata],
+  metadata: metadata.sp[instance],
 })
 
 module.exports = (client, request, response) => {
   sp.parseLoginResponse(idp, 'post', request)
     .then(parseResult => {
-      client.response.set({
+      response.set({
         'Set-Cookie': `csumbUser=${JSON.stringify(
           parseResult.extract.attributes
-        )}; Domain=${host.domain}${host.secure}`,
+        )}; Domain=${host.domain}; Path=/${host.secure}`,
       })
-      response.redirect(host.redirect)
-      response.end()
+      bcrypt.hash(parseResult.extract.attributes.login + salt, 10, function(
+        err,
+        hash
+      ) {
+        response.set({
+          'Set-Cookie': `csumbSession=${hash}; Domain=${host.domain}; Path=/${
+            host.secure
+          }`,
+        })
+        if (instance === 'local') {
+          console.log(
+            `csumbUser=${JSON.stringify(parseResult.extract.attributes)}`
+          )
+          console.log(`csumbSession=${hash}`)
+        }
+        response.redirect(host.redirect)
+        response.end()
+      })
     })
     .catch(error => {
-      response.send(JSON.stringify(error))
+      console.log(error)
+      response.redirect(host.redirect)
       response.end()
     })
 }
