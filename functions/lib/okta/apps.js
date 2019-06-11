6679704269b2b01556e4637f8cb0aa1d8ede61a4
Kevin Miller <kevin@csumb.edu>
2019-06-11 12:22:54 -0300

    Remove test

--- functions/lib/okta/apps.js
+++ functions/lib/okta/apps.js
@@ -1,6 +1,6 @@
 const checkHash = require('../checkHash')
 
-module.exports = (client, request, response) => {
+module.exports = async (client, request, response) => {
   if (!checkHash(request)) {
     response.write(JSON.stringify({ error: true }))
     response.end()
@@ -8,17 +8,14 @@ module.exports = (client, request, response) => {
   }
 
   const apps = []
-  const getApps = async () => {
-    await client.listAppLinks(request.query.user).each(app => {
-      apps.push({
-        id: app.id,
-        label: app.label,
-        linkUrl: app.linkUrl,
-        sortOrder: app.sortOrder,
-      })
+  await client.listAppLinks(request.query.user).each(app => {
+    apps.push({
+      id: app.id,
+      label: app.label,
+      linkUrl: app.linkUrl,
+      sortOrder: app.sortOrder,
     })
-    response.write(JSON.stringify(apps))
-    response.end()
-  }
-  getApps()
+  })
+  response.write(JSON.stringify(apps))
+  response.end()
 }
