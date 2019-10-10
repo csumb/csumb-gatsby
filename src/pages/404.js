b8e8c786305fdfcebf8eb77b3ea97521ad938460
Kevin Miller <keveemiller@gmail.com>
2019-10-10 14:11:40 -0300

    Added search to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,12 +5,18 @@ import { LeadParagraph } from '../components/common/type'
 import Brand from '../components/layouts/sections/header/brand'
 import styled from '@emotion/styled'
 import Link from 'gatsby-link'
+import { Flex, Box } from '../components/common/grid'
+import { InputText, Submit } from '../components/common/forms'
 import { EverythingContent } from '../components/pages/everything'
 import { graphql } from 'gatsby'
 
 const PageNotFoundContainer = styled.div`
   max-width: 60ch;
   margin: 3rem auto;
+`
+
+const EverythingItems = styled.div`
+  margin-top: 2rem;
 `
 
 class ErrorPage extends Component {
@@ -40,20 +46,38 @@ class ErrorPage extends Component {
             We couldn't find that page, but here's a great way to find what
             you're looking for:
           </LeadParagraph>
-          {items.map(edge => (
-            <>
-              {edge.node.topLevelItem && (
-                <>
-                  <h3>
-                    <Link to={`/everything/${edge.node.slug}`}>
-                      {edge.node.title}
-                    </Link>
-                  </h3>
-                  <EverythingContent item={edge.node} />
-                </>
-              )}
-            </>
-          ))}
+          <form method="GET" action="/search">
+            <Flex>
+              <Box width={[1, 2 / 3]} px={2}>
+                <InputText
+                  name="q"
+                  label="Search"
+                  huge
+                  hideLabel
+                  placeholder="Search CSUMB"
+                />
+              </Box>
+              <Box width={[1, 1 / 3]} px={2}>
+                <Submit value="Search" huge nomargin />
+              </Box>
+            </Flex>
+          </form>
+          <EverythingItems>
+            {items.map(edge => (
+              <>
+                {edge.node.topLevelItem && (
+                  <>
+                    <h3>
+                      <Link to={`/everything/${edge.node.slug}`}>
+                        {edge.node.title}
+                      </Link>
+                    </h3>
+                    <EverythingContent item={edge.node} />
+                  </>
+                )}
+              </>
+            ))}
+          </EverythingItems>
         </PageNotFoundContainer>
       </PlainLayout>
     )
