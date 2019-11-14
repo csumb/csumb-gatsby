d6ee0f1ad6fa30b1185da167a7c54b86050a0b23
Kevin Miller <keveemiller@gmail.com>
2019-11-14 20:48:56 -0300

    Updated netlify

--- netlify.sh
+++ netlify.sh
@@ -1,9 +1,3 @@
-git clone --depth 1 https://$GITHUB_TOKEN@github.com/csumb/web-data.git _data
-git clone --depth 1 https://$GITHUB_TOKEN@github.com/csumb/web-content.git _web-content
+git clone --depth 1 https://$GITHUB_TOKEN@github.com/csumb/website-data.
 node --max_old_space_size=12000 ./node_modules/.bin/gatsby build
-npm run directoryJson
-npm run scheduleJson
-npm run catalogJson
-npm run eventJson
-npm run buildJson
 npm run canary
