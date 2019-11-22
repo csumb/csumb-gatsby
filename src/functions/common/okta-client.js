861b9cc88ae99e9f2aaa5a3cd3b565da3322336f
Kevin Miller <keveemiller@gmail.com>
2019-11-22 09:50:49 -0300

    Change to absolute URLs

--- src/functions/common/okta-client.js
+++ src/functions/common/okta-client.js
@@ -1,6 +1,6 @@
 import oktaClient from '@okta/okta-sdk-nodejs'
 
 export default new oktaClient.Client({
-  orgUrl: 'csumb.okta.com',
+  orgUrl: 'https://csumb.okta.com',
   token: process.env.CSUMB_FUNCTIONS_OKTA_KEY,
 })
