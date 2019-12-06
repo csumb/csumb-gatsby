14a670db8a85ff3c797ac6b67341234d9a184db5
Kevin Miller <keveemiller@gmail.com>
2019-12-06 10:27:27 -0300

    Cleanup login name

--- src/functions/functions/login/sp.js
+++ src/functions/functions/login/sp.js
@@ -1,7 +1,7 @@
 export default {
   entity_id: 'https://csumb.edu/',
   audience: 'https://csumb.edu/',
-  assert_endpoint: 'https://csumb.edu/.netlify/functions/okta-login',
+  assert_endpoint: 'https://csumb.edu/.netlify/functions/login',
   private_key: '',
   certificate: '',
 }
