019e2b019d6f4a509c23413e78874093ff8fd54b
Kevin Miller <keveemiller@gmail.com>
2019-12-06 22:32:20 -0300

    Fixed redirects

--- plugins/gatsby-source-csumb-content-api-redirects/gatsby-node.js
+++ plugins/gatsby-source-csumb-content-api-redirects/gatsby-node.js
@@ -17,12 +17,18 @@ exports.sourceNodes = async (
   const redirects = JSON.parse(response.getBody())
 
   Object.keys(redirects.redirects).forEach(source => {
+    const sourceAddress =
+      source.search('://') > -1 ? source : '/' + source.replace(/^\//g, '')
+    const target =
+      redirects.redirects[source].search('://') > -1
+        ? redirects.redirects[source]
+        : '/' + redirects.redirects[source].replace(/^\//g, '')
     let redirectNode = {
       id: createNodeId(`${source} >>> CsumbContentRedirect`),
       children: [],
       redirect: {
-        source: `/${source}`,
-        target: `/${redirects.redirects[source]}`,
+        source: sourceAddress,
+        target: target,
       },
       parent: null,
       internal: {
