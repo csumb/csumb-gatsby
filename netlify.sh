59ecde4ad50109c633b93a3b7029d2cb08baaef4
Kevin Miller <keveemiller@gmail.com>
2019-12-09 08:37:31 -0300

    Change error

--- netlify.sh
+++ netlify.sh
@@ -1,5 +1,5 @@
 rm -rf website-data || true
 git clone --depth 1 https://$GITHUB_TOKEN@github.com/csumb/website-data
 node --max_old_space_size=12000 ./node_modules/.bin/gatsby build
-npm run canary || error 4
+npm run canary || error 1
 npm run buildFunctions
