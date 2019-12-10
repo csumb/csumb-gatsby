4011e5daca4582ca4264e1c4d1ddc3f76008e45c
Kevin Miller <keveemiller@gmail.com>
2019-12-10 13:19:59 -0300

    Remove hash for now

--- src/functions/functions/okta-apps/index.js
+++ src/functions/functions/okta-apps/index.js
@@ -1,15 +1,6 @@
 import client from '../../common/okta-client'
-import checkHash from '../../common/check-hash'
 
 exports.handler = async (event, context, callback) => {
-  if (!checkHash(event)) {
-    callback(null, {
-      statusCode: 403,
-      body: JSON.stringify({ error: true }),
-    })
-    return
-  }
-
   const apps = []
   await client.listAppLinks(event.queryStringParameters.user).each(app => {
     apps.push({
