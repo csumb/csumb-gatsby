1e123ebd746dc4170c184a9793e6235c842b2726
Kevin Miller <keveemiller@gmail.com>
2019-12-12 07:51:05 -0300

    Add status code to logout

--- src/functions/functions/logout/index.js
+++ src/functions/functions/logout/index.js
@@ -1,8 +1,10 @@
 exports.handler = (event, context, callback) => {
   callback(null, {
     headers: {
+      statusCode: 301,
       Location: 'https://csumb.edu/',
-      'Set-Cookie': 'csumbWebUser=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'
-    }
+      'Set-Cookie':
+        'csumbWebUser=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT',
+    },
   })
 }
