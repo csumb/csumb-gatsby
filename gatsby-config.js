5b0255818b03e4dd215d4edbea1a32c420242a1a
codywall <cwall@csumb.edu>
2019-12-18 12:11:56 -0800

    Revert "Test failed airtable"
    This reverts commit 98f6252eff532030e0dcac5bac6f62036de2d527.

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,7 +131,7 @@ const gatsbyConfig = {
         tables: [
           // Science illustration
           {
-            baseId: 'appW5yp8SpOvxD3AlTEST',
+            baseId: 'appW5yp8SpOvxD3Al',
             tableName: 'Graduates',
             queryName: 'ScienceIllustrationGraduates',
           },
