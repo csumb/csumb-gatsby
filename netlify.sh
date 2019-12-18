c68a6514ed01d0c7fa816c7e57904a995b1131f5
codywall <cwall@csumb.edu>
2019-12-18 10:59:54 -0800

    Exit build command on error

--- netlify.sh
+++ netlify.sh
@@ -1,5 +1,6 @@
 rm -rf website-data || true
 git clone --depth 1 https://$GITHUB_TOKEN@github.com/csumb/website-data
+set -e
 node --max_old_space_size=12000 ./node_modules/.bin/gatsby build
 npm run canary || error 1
 npm run buildFunctions
