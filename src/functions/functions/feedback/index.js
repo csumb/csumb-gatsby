e762ffeff34dae807d9278752de288b6a54fcfad
codywall <cwall@csumb.edu>
2020-01-06 14:26:38 -0800

    Fixed feedback function

--- src/functions/functions/feedback/index.js
+++ src/functions/functions/feedback/index.js
@@ -3,15 +3,15 @@ import sgMail from '@sendgrid/mail'
 
 sgMail.setApiKey(process.env.CSUMB_FUNCTIONS_SENDGRID)
 
-exports.handler = (event, context, callback) => {
-  const feedbackEmail = event.queryStringParameters.feedbackEmail
+module.exports = (request, response) => {
+  const feedbackEmail = request.query.feedbackEmail
 
-  const feedbackHTML = feedbackTemplate(event.queryStringParameters)
+  const feedbackHTML = feedbackTemplate(request.query)
   const msg = {
     to: feedbackEmail,
     from: 'webservices@csumb.edu',
     cc: 'webservices@csumb.edu',
-    subject: `Page feedback on ${event.queryStringParameters.title}`,
+    subject: `Page feedback on ${request.query.title}`,
     html: feedbackHTML,
   }
   sgMail.send(msg)
