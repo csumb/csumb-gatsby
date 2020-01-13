0c5cc02418671b26d04a32d8c54eee7bbce9fed7
codywall <cwall@csumb.edu>
2020-01-13 10:18:26 -0800

    Changed feedback template naming

--- src/functions/functions/feedback/index.js
+++ src/functions/functions/feedback/index.js
@@ -1,17 +1,17 @@
-const feedbackTemplate = require('./template')
+const feedbackTemplate = require('./template.js')
 const sgMail = require('@sendgrid/mail')
 
 setApiKey(process.env.CSUMB_FUNCTIONS_SENDGRID)
 
-export default (request, response) => {
-  const feedbackEmail = request.query.feedbackEmail
+export default (event, context, callback) => {
+  const feedbackEmail = event.queryStringParameters.feedbackEmail
 
-  const feedbackHTML = feedbackTemplate(request.query)
+  const feedbackHTML = feedbackTemplate(event.queryStringParameters)
   const msg = {
     to: feedbackEmail,
     from: 'webservices@csumb.edu',
     cc: 'webservices@csumb.edu',
-    subject: `Page feedback on ${request.query.title}`,
+    subject: `Page feedback on ${event.queryStringParameters.title}`,
     html: feedbackHTML,
   }
   sgMail
