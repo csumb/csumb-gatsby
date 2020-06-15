56c15af1187ded413600f68d4f311a29ab2423d3
codywall <cwall@csumb.edu>
2020-06-15 16:14:34 -0700

    Add SATfollowup form and put forms into folder

---
+++ src/pages/forms/SATfollowup.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import {
+  Layout,
+  SiteHeader,
+  SiteNavigation,
+  PageTitle,
+} from '../../components/layouts/default'
+import { graphql } from 'gatsby'
+import Container from '../../components/common/container'
+import Iframe from 'react-iframe'
+import styled from '@emotion/styled'
+
+const Disclaimer = styled('div')`
+  min-width: 80%;
+  p {
+    font-size: 0.75rem;
+  }
+`
+
+const InquiryPage = ({ location }) => {
+  const [pid, setPid] = useState('')
+  useEffect(
+    () => {
+      setPid(location.search)
+    },
+    [location]
+  )
+  return (
+    <Layout pageTitle="Recruitment Follow Up">
+      <SiteHeader path="/admissions">Admissions</SiteHeader>
+      <SiteNavigation navigation={null} />
+      <Container>
+        <PageTitle>Cal State Monterey Bay Follow Up</PageTitle>
+        <div id="iFrameWrapper">
+          <Iframe
+            src={`https://csumb.tfaforms.net/15${pid}`}
+            id="followUp"
+            height="auto"
+            width="100%"
+            frameBorder="0"
+          />
+          <Disclaimer>
+            <p>
+              By submitting this form you are granting CSUMB permission to email
+              you information regarding the university.
+            </p>
+          </Disclaimer>
+        </div>
+        <script src="//csumb.tfaforms.net/js/iframe_resize_helper.js" />
+      </Container>
+    </Layout>
+  )
+}
+
+export default InquiryPage
+
+export const query = graphql`
+  {
+    allMarkdownRemark(
+      filter: { frontmatter: { name: { eq: "recruitmentFollowup" } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            name
+            title
+          }
+          html
+        }
+      }
+    }
+  }
+`
