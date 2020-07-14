7c04f2ce9f9cae0bc36fb928faee58af3d56d59c
codywall <cwall@csumb.edu>
2020-07-14 09:30:53 -0700

    Move chat to within columns

--- src/pages/library/index.js
+++ src/pages/library/index.js
@@ -98,7 +98,7 @@ class LibraryPage extends Component {
       return
     }
     const script = window.document.createElement('script')
-    script.src = 'https://v2.libanswers.com/load_chat.php?hash=eeeaabbd6b1739014e7e8fe4e6db5bed'
+    script.src = 'https://v2.libanswers.com/load_chat.php?hash=addeae1f890e28920bfda5ac4c0a6e26'
     this.chatRef.current.parentNode.insertBefore(script, this.chatRef.current)
   }
 
@@ -127,10 +127,8 @@ class LibraryPage extends Component {
               />
             )}
           <Container>
-            <LibrarySearch />
+            <LibrarySearch />        
             <div ref={this.chatRef} />
-            <div id="libchat_eeeaabbd6b1739014e7e8fe4e6db5bed"></div>
-            <style>{`.libraryh3lp img { height: 44px !important;}`}</style>
 
             {data.allCsumbPage &&
               data.allCsumbPage.edges &&
