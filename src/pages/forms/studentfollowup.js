528c77d1a684c7e23b2f1c44bd741226a072389a
codywall <cwall@csumb.edu>
2020-08-05 13:11:54 -0700

    Add page feedback to forms pages

--- src/pages/forms/studentfollowup.js
+++ src/pages/forms/studentfollowup.js
@@ -8,6 +8,7 @@ import {
 import { graphql } from 'gatsby'
 import Container from '../../components/common/container'
 import IframeResizer from 'iframe-resizer-react'
+import PageFeedbackContext from '../../components/contexts/page-feedback'
 
 const InquiryPage = ({ location }) => {
   const [pid, setPid] = useState('')
@@ -18,21 +19,29 @@ const InquiryPage = ({ location }) => {
     [location]
   )
   return (
-    <Layout pageTitle="Recruitment Follow Up">
-      <SiteHeader path="/admissions">Admissions</SiteHeader>
-      <SiteNavigation navigation={null} />
-      <Container>
-        <PageTitle>Cal State Monterey Bay Follow Up</PageTitle>
-        <IframeResizer
-          src={`https://csumb.tfaforms.net/15${pid}`}
-          frameBorder="0"
-          style={{ width: '1px', minWidth: '100%', marginBottom: 0 }}
-          sandbox="allow-top-navigation allow-same-origin allow-forms allow-popups allow-scripts"
-          target="_top"
-        />
-        <script src="https://csumb.tfaforms.net/js/iframe_resize_helper.js" />
-      </Container>
-    </Layout>
+    <PageFeedbackContext.Provider
+      value={{
+        email: 'webfolk@csumb.edu',
+        title: 'Student followup form',
+        url: '/forms/studentfollowup',
+      }}
+    >
+      <Layout pageTitle="Recruitment Follow Up">
+        <SiteHeader path="/admissions">Admissions</SiteHeader>
+        <SiteNavigation navigation={null} />
+        <Container>
+          <PageTitle>Cal State Monterey Bay Follow Up</PageTitle>
+          <IframeResizer
+            src={`https://csumb.tfaforms.net/15${pid}`}
+            frameBorder="0"
+            style={{ width: '1px', minWidth: '100%', marginBottom: 0 }}
+            sandbox="allow-top-navigation allow-same-origin allow-forms allow-popups allow-scripts"
+            target="_top"
+          />
+          <script src="https://csumb.tfaforms.net/js/iframe_resize_helper.js" />
+        </Container>
+      </Layout>
+    </PageFeedbackContext.Provider>
   )
 }
 
--- src/pages/forms/tellusmore.js
+++ src/pages/forms/tellusmore.js
@@ -8,6 +8,7 @@ import {
 import { graphql } from 'gatsby'
 import Container from '../../components/common/container'
 import IframeResizer from 'iframe-resizer-react'
+import PageFeedbackContext from '../../components/contexts/page-feedback'
 
 const InquiryPage = ({ location }) => {
   const [pid, setPid] = useState('')
@@ -18,21 +19,29 @@ const InquiryPage = ({ location }) => {
     [location]
   )
   return (
-    <Layout pageTitle="Tell us more">
-      <SiteHeader path="/admissions">Admissions</SiteHeader>
-      <SiteNavigation navigation={null} />
-      <Container>
-        <PageTitle>Tell us a little more about yourself</PageTitle>
-        <IframeResizer
-          src={`https://csumb.tfaforms.net/16${pid}`}
-          frameBorder="0"
-          style={{ width: '1px', minWidth: '100%', marginBottom: 0 }}
-          sandbox="allow-top-navigation allow-same-origin allow-forms allow-popups allow-scripts"
-          target="_top"
-        />
-        <script src="https://csumb.tfaforms.net/js/iframe_resize_helper.js" />
-      </Container>
-    </Layout>
+    <PageFeedbackContext.Provider
+      value={{
+        email: 'webfolk@csumb.edu',
+        title: 'Tell us more form',
+        url: '/forms/tellusmore',
+      }}
+    >
+      <Layout pageTitle="Tell us more">
+        <SiteHeader path="/admissions">Admissions</SiteHeader>
+        <SiteNavigation navigation={null} />
+        <Container>
+          <PageTitle>Tell us a little more about yourself</PageTitle>
+          <IframeResizer
+            src={`https://csumb.tfaforms.net/16${pid}`}
+            frameBorder="0"
+            style={{ width: '1px', minWidth: '100%', marginBottom: 0 }}
+            sandbox="allow-top-navigation allow-same-origin allow-forms allow-popups allow-scripts"
+            target="_top"
+          />
+          <script src="https://csumb.tfaforms.net/js/iframe_resize_helper.js" />
+        </Container>
+      </Layout>
+    </PageFeedbackContext.Provider>
   )
 }
 
