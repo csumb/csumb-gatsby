0795409ce77ce8ddb3c20a616fe75e9e04912d69
codywall <cwall@csumb.edu>
2020-08-10 11:49:52 -0700

    Stringify the props objects

--- src/pages/diploma-test/index.js
+++ src/pages/diploma-test/index.js
@@ -11,18 +11,22 @@ import PageFeedbackContext from '../../components/contexts/page-feedback'
 import { UserContext } from '../../components/contexts/user'
 
 const DiplomaPage = props => {
-  console.log(`Props: ${props}`)
+  console.log(`Props: ${JSON.stringify(props)}`)
   return (
     <PageFeedbackContext.Provider
       value={{ email: 'webfolk@csumb.edu', title: 'Diploma', url: '/diploma' }}
     >
-      <Layout pageTitle="Inquiry Form">
-        <SiteHeader path="/admissions">Admissions</SiteHeader>
+      <Layout pageTitle="Test">
+        <SiteHeader path="/admissions">Test</SiteHeader>
         <SiteNavigation navigation={null} />
         <Container>
           <PageTitle>This page is for testing purposes only</PageTitle>
           <UserContext.Consumer>
-            {context => console.log(`UserContext.Consumer: ${context.user}`)}
+            {context =>
+              console.log(
+                `UserContext.Consumer: ${JSON.stringify(context.user)}`
+              )
+            }
           </UserContext.Consumer>
         </Container>
       </Layout>
