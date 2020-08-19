10c6731c23584624c409528b329e77ee76514f77
codywall <cwall@csumb.edu>
2020-08-19 11:57:10 -0700

    Initial styling of credential validation

--- src/pages/web/validation-test.js
+++ src/pages/web/validation-test.js
@@ -13,6 +13,10 @@ import { InputText, Submit } from '../../components/common/forms'
 import styled from '@emotion/styled'
 import Blocks from '../../templates/blocks'
 import PageFeedbackContext from '../../components/contexts/page-feedback'
+import { AlertWarning } from '../../components/common/alert'
+import { Button } from '../../components/common/button'
+import { colors } from '../../style'
+import credentialLogo from '../../assets/images/cecredential-logo.png'
 
 const initialFormData = Object.freeze({
   id: '',
@@ -32,19 +36,60 @@ const resultTable = credential => {
       </ul>
     </div>
   ) : (
-    <h5>This is not a valid credential.</h5>
+    <AlertWarning>
+      <p>
+        We cannot validate the Credential at this time.
+        <br />
+        The information provided does not match the information on record, or
+        there was a connection error.
+        <br />
+        Please contact *REGISTRAR@SAMPLEUNIVERSITY.EDU* for assistance. When you
+        do, please provide the student name and CeDiD.
+      </p>
+    </AlertWarning>
   )
 }
 
-const CredentialForm = () => {
+const LabelInputWrapper = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding-bottom: 20px;
+`
+
+const Label = styled('label')`
+  width: 40%;
+  font-size: 0.9rem;
+  border-radius: 2px;
+  padding: 5px;
+  background: ${colors.muted.light};
+`
+
+const ValidateInput = styled('input')`
+  max-width: 50%;
+  max-height: 3rem;
+  padding: 5px 10px;
+  border-radius: 2px;
+  border: 1px solid ${colors.gray.default};
+  padding: 0.3rem;
+  &:focus {
+    transition: all 100ms;
+    outline: 0.15rem solid ${colors.primary.default};
+  }
+`
+
+const CredentialValidation = () => {
   const [formData, updateFormData] = useState(initialFormData)
   const [data, setData] = useState({ credential: {} })
 
   const handleChange = e => {
+    var foo = e.target.value.split('-').join('')
+    if (foo.length > 0) {
+      foo = foo.match(new RegExp('.{1,4}', 'g')).join('-')
+    }
+    e.target.value = foo
     updateFormData({
       ...formData,
-
-      // Trimming any whitespace
       [e.target.name]: e.target.value.trim(),
     })
   }
@@ -75,17 +120,34 @@ const CredentialForm = () => {
 
   return (
     <>
-      <label>
-        ID
-        <input name="id" onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-        First two of name
-        <input name="name" onChange={handleChange} />
-      </label>
-      <br />
-      <button onClick={handleSubmit}>Submit</button>
+      <Flex style={{ maxWidth: '500px', marginBottom: '20px' }}>
+        <LabelInputWrapper>
+          <Label htmlFor="id">
+            Please enter CeDiD <br />
+            <span>(not case sensitive)</span>
+          </Label>
+          <ValidateInput
+            maxLength="14"
+            name="id"
+            placeholder="_ _ _ _ - _ _ _ _ - _ _ _ _"
+            onChange={handleChange}
+          />
+        </LabelInputWrapper>
+        <LabelInputWrapper>
+          <Label htmlFor="name">
+            Enter the first two letters of the name as it appears on the
+            credential
+          </Label>
+          <ValidateInput
+            maxLength="2"
+            name="name"
+            placeholder="_ _"
+            onChange={handleChange}
+          />
+        </LabelInputWrapper>
+        <Button onClick={handleSubmit}>Validate</Button>
+      </Flex>
+      <img src={credentialLogo} alt="Powered by CeCredential Trust" />
       {Object.keys(data.credential).length !== 0 &&
         resultTable(data.credential)}
     </>
@@ -115,7 +177,7 @@ class ITPage extends Component {
               />
             )}
           <Container topPadding>
-            <CredentialForm />
+            <CredentialValidation />
             {data.allCsumbPage &&
               data.allCsumbPage.edges &&
               data.allCsumbPage.edges[0] && (
