557d25bdf2567a6220b7b85cfe9477d8c0e49998
codywall <cwall@csumb.edu>
2021-02-23 15:17:24 -0800

    Use useEffect to get document

--- src/pages/grad-day.js
+++ src/pages/grad-day.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   Layout,
   SiteHeader,
@@ -10,43 +10,28 @@ import Container from '../components/common/container'
 import IframeResizer from 'iframe-resizer-react'
 import PageFeedbackContext from '../components/contexts/page-feedback'
 
-const html = `<script>
+const GradDayPage = () => {
+  const html = `<script>
         <!--//--><![CDATA[// ><!--
+        
+        (function (d) {
+            // embed.js CDN
+            var cdn = window.StacklaGoConnectConfig ? window.StacklaGoConnectConfig.cdn : 'assetscdn.stackla.com';
+            var t = d.createElement('script');
+            t.type = 'text/javascript';
+            t.src = 'https://' + cdn + '/media/js/common/plugin_goconnect_embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(t);
+        }(document));
 
-(function (d) {
-    // embed.js CDN
-    var cdn = window.StacklaGoConnectConfig ? window.StacklaGoConnectConfig.cdn : 'assetscdn.stackla.com';
-    var t = d.createElement('script');
-    t.type = 'text/javascript';
-    t.src = 'https://' + cdn + '/media/js/common/plugin_goconnect_embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(t);
-}(document));
-
-//--><!]]>
+        //--><!]]>
     </script>`
 
-const ScriptEl = document.createRange().createContextualFragment(html)
-document.body.append(ScriptEl)
-
-// const GradScript = document.createElement('script')
-// GradScript.setAttribute('type', 'text/javascript')
-// GradScript.text = `
-// <!--//--><![CDATA[// ><!--
-
-// (function (d) {
-//     // embed.js CDN
-//     var cdn = window.StacklaGoConnectConfig ? window.StacklaGoConnectConfig.cdn : 'assetscdn.stackla.com';
-//     var t = d.createElement('script');
-//     t.type = 'text/javascript';
-//     t.src = 'https://' + cdn + '/media/js/common/plugin_goconnect_embed.js';
-//     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(t);
-// }(document));
-
-// //--><!]]>
-// `
-// target.appendChild(GradScript)
-
-const GradDayPage = () => {
+  function ScriptEl() {
+    document.createRange().createContextualFragment(html)
+    useEffect(() => {
+      document.body.prepend(ScriptEl)
+    })
+  }
   return (
     <PageFeedbackContext.Provider
       value={{
@@ -61,7 +46,7 @@ const GradDayPage = () => {
         <Container>
           <PageTitle>Grad Day (test)</PageTitle>
           <h2>CSUMB Grad Day</h2>
-          {/* <ScriptEl /> */}
+          <ScriptEl />
           <IframeResizer
             src={`https://goconnect.stackla.com/widget/show?plugin_id=5ea196be5374c`}
             frameBorder="0"
